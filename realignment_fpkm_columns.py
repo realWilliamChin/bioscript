a814bbf25508d1e042176f3b044d31136bc685ce
WilliamGoGo <realwilliamchin@outlook.com>
2023-06-26 11:19:30 +0800

    realignment_fpkm_columns.py add check_columns_name feature

--- realignment_fpkm_columns.py
+++ realignment_fpkm_columns.py
@@ -13,8 +13,17 @@ def parse_input():
     parser.add_argument('-r', '--replacefile', action='store_true', help='是否对源文件替换，不替换则会重新生成 _realign.txt')
     args = parser.parse_args()
     return args.sample, args.file, args.replacefile
-    
 
+
+def check_columns_name(lst, df_columns):
+    for i in lst:
+        if i == df_columns[0]:
+            continue
+        if not i in df_columns:
+            print(f'{i} not in {df_columns}')
+            exit(1)
+    
+    
 def reindex(lst, file, replace_or_not):
     df = pd.read_csv(file, sep='\t')
     lst.insert(0, df.columns[0])
@@ -24,7 +33,7 @@ def reindex(lst, file, replace_or_not):
         df.reindex(columns=lst).to_csv(file.replace('.txt', '_realign.txt'), sep='\t', index=False)
 
 
-if __name__ == '__main__':
+def main():
     parse_input = parse_input()
     samples_file = parse_input[0]
     realign_file = parse_input[1]
@@ -32,4 +41,9 @@ if __name__ == '__main__':
     
     sample_arr = list(pd.read_csv(samples_file, sep='\t')['sample'])
     sample_arr
+    check_columns_name(sample_arr, pd.read_csv(realign_file, sep='\t').columns)
     reindex(sample_arr, realign_file, replace_ornot)
+
+
+if __name__ == '__main__':
+    main()
