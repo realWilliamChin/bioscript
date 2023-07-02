d6f382af52bbac7a894ad742a962b1fb3727ab0b
WilliamGoGo <realwilliamchin@outlook.com>
2023-07-02 22:45:06 +0800

    [BUG FIXED] geneid_add_goid.py fix parse_input bug and improve some code

--- geneid_add_goid.py
+++ geneid_add_goid.py
@@ -9,7 +9,7 @@ from get_wego_pic import get_wego_pic
 
 def parse_input():
     argparser = argparse.ArgumentParser(description='')
-    argparser.add_argument('-a', '--all', default='all_geneid_Go.txt',
+    argparser.add_argument('-a', '--all', default='all_geneid_GO.txt',
                            help='指定所有基因的 GO 文件, 默认当前文件夹下的 _all_geneid_Go 文件')
     argparser.add_argument('-f', '--file',
                            help='指定基因的 ID 文件(结尾必须是_ID.txt), 默认当前文件夹下的 Up Down_ID.txt 文件')
@@ -19,9 +19,6 @@ def parse_input():
                            help='是否生成图片，默认不生成')
     argparser.add_argument('-l', '--level', type=str, default='3', help='指定 GO 的 level, 默认 3, 与 -p 一块儿使用')
     args = argparser.parse_args()
-    if not args.picture and args.level:
-        print('如果不生成图片，就不用指定 go level')
-        exit()
     return args
 
 
@@ -32,21 +29,18 @@ def geneid_goid(all_geneid_goid_file, updownid_file, generate_type):
     updown_go_filename = updownid_file.replace('_ID.txt', '_Go.txt')
     # 读取 updownid_file 为 list
     print(f'reading {updownid_file}, generating {updown_go_filename} ...')
-    with open(updownid_file, 'r') as file:
-        id_lst = list(file.read().splitlines())
+    with open(updownid_file, 'r') as id_lst_file:
+        id_lst = list(id_lst_file.read().splitlines())
     # 循环读取 all_geneid_goid_file，如果 GeneID 在 id_lst 中，就写入 updown_go_filename
-    with open(all_geneid_goid_file, 'r') as file:
-        for line in file:
+    with open(all_geneid_goid_file, 'r') as input_file, open(updown_go_filename, 'w') as updown_go_file:
+        for line in input_file:
             geneid = line.split('\t')[0]
             if geneid in id_lst:
-                with open(updown_go_filename, 'a') as updown_go_file:
-                    # 如果是 wego_genomics，就直接写入，如果是 tbtools，就把除第一个 tab 换成逗号
-                    if generate_type == 'wego_genomics':
-                        updown_go_file.write(line)
-                    elif generate_type == 'tbtools':
-                        line = line.replace('\t', ',')
-                        line = line.replace(',', '\t', 1)
-                        updown_go_file.write(line)
+                # 如果是 wego_genomics，就直接写入，如果是 tbtools，就把除第一个 tab 换成逗号
+                if generate_type == 'tbtools':
+                    line = line.replace('\t', ',')
+                    line = line.replace('\t', ',', 1)
+                updown_go_file.write(line)
     return updown_go_filename
 
 
