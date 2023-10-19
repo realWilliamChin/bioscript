59ec9676177a67d43b8d76ed6bbc533fa35306bc
WilliamGoGo <realwilliamchin@outlook.com>
2023-10-19 14:54:09 +0800

    [REFACTOR] transcriptome/annotation/nr.py 重写，使用 Pandas，并对 nr_TF_def 的 NR_Def 列按照字母排序

--- transcriptome/annotation/nr.py
+++ transcriptome/annotation/nr.py
@@ -1,133 +1,47 @@
-#!/home/train/miniconda3/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Created Time  : 2023/06/14 17:05
 # Author        : William GoGo
+"""
+nr 注释程序
+生成 nr.blast 文件，nr_gene_def.txt 文件 和 nr_TF_def.txt 文件
+"""
 import argparse
 import os
-from shutil import copyfile
-import sys
-import numpy
-import re
 import pandas as pd
 
-######################################################################################################################
-###输入
-input=os.getcwd()
-parser=argparse.ArgumentParser(description="nr")
-parser.add_argument('--input',dest='input',default=input,help='path of nr.blast')
 
-arg=parser.parse_args()
-
-
-dic_nr={}
-cur_path=arg.input
-
-output_path=cur_path
-#output_path=cur_path+os.sep+'swiss_delete_duplicate_all_info'
-isExists = os.path.exists(output_path)
-
-# 判断结果
-if not isExists:
-
-    os.makedirs(output_path)
-
-temp=output_path+os.sep+'temp'
-isExists = os.path.exists(temp)
-
-# 判断结果
-if not isExists:
-
-    os.makedirs(temp)
-dir_list=os.listdir(cur_path)
-for each_doc in dir_list:
-    if ('nr.blast' in each_doc):
-    #if ('swiss.blast' in each_doc):
-        new_name1=each_doc.replace('.blast','_uniq.blast')
-        new_name2 = each_doc.replace('.blast', '_after_sort_all_info.txt')
-        # new_name3 = each_doc.replace('.blast', '_nr_gene_def.txt')
-        # new_name4 = each_doc.replace('.blast','_nr_TF_def.txt')
-        new_name3 = each_doc.replace('nr.blast', 'nr_gene_def.txt')
-        new_name4 = each_doc.replace('nr.blast','nr_TF_def.txt')
-        data_frame = pd.read_csv(cur_path+os.sep+each_doc, header=None ,sep='\t')  # 获取日期数据
-        print(data_frame)
-        data_frame2=data_frame.sort_values(by=[0, 2],ascending=[True,False])
-
-        data_frame2.to_csv(path_or_buf=temp+os.sep+new_name2,sep='\t',header=None, index = False)
+def parse_input():
+    args = argparse.ArgumentParser(description='输入 nr.blast 文件的路径')
+    args.add_argument('-i', '--input', type=str, help='nr.blast file')
+    
+    return args.parse_args()
 
 
-dic_nr1={}
-dic_nr2={}
-f1=open(temp+os.sep+new_name2,'r')
-f2=open(output_path+os.sep+new_name1,'w')
-f2.write('qseqid'+'\t'+ 'sseqid'+'\t'+ 'pident'+'\t'+ 'length'+'\t'+ 'mismatch'+'\t'+ 'gapopen'+'\t' +'qstart'+'\t'+ 'qend''\t' +'sstart'+'\t' +'send' +'\t'+'evalue' +'\t'+'bitscore' +'\t'+'stitle'+'\n')
-f3=open(output_path+os.sep+new_name3,'w')
-f3.write('GeneID'+'\t'+'NCBI_ID'+'\t'+'NR_Def'+'\n')
+def nr(nr_blast_file):
+    columns = ['qseqid', 'sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend','sstart','send','evalue','bitscore','stitle']
 
-f4=open(output_path+os.sep+new_name4,'w')
-f4.write('GeneID'+'\t'+'NCBI_ID'+'\t'+'NR_Def'+'\n')
+    data_frame = pd.read_csv(nr_blast_file, sep='\t', names=columns)
 
-for each_line in f1:
-    id=each_line.split('\t')[0]
-    content1=each_line.split('\t',1)[-1]
-    content2 = each_line.split('\t')[-1]
-    if id in dic_nr1:
-        continue
-    else:
-        dic_nr1[id]=content1
-        dic_nr2[id]=content2.split(' ')[0]+'\t'+content2.split(' ',1)[-1]
-for each_key in dic_nr1:
-    f2.write(each_key+'\t'+dic_nr1[each_key])
-for each_key in dic_nr2:
-    f3.write(each_key+'\t'+dic_nr2[each_key])
-    if 'transcription' in dic_nr2[each_key]:
-        f4.write(each_key+'\t'+dic_nr2[each_key])
-
-f1.close()
-f2.close()
-f3.close()
-f4.close()
+    data_frame = data_frame.sort_values(by=['qseqid', 'pident'], ascending=[True, False])
+    data_frame = data_frame.drop_duplicates(subset=['qseqid'], keep='first')
+    nr_gene_def_df = data_frame[['qseqid', 'sseqid', 'stitle']].copy()
+    nr_gene_def_df.columns = ['GeneID', 'NCBI_ID', 'NR_Def']
+    nr_gene_def_df['NR_Def'] = nr_gene_def_df['NR_Def'].str.split(n=1).str[1]
+    nr_gene_def_df.to_csv(nr_blast_file.replace('.blast', '_gene_def.txt'), sep='\t', index=False)
+    nr_TF_def_df = nr_gene_def_df[nr_gene_def_df['NR_Def'].str.contains('transcription')]
+    nr_TF_def_df = nr_TF_def_df.sort_values(by='NR_Def', key=lambda x: x.str.lower())
+    nr_TF_def_df.to_csv(nr_blast_file.replace('.blast', '_TF_def.txt'), sep='\t', index=False)
 
 
+def main():
+    args = parse_input()
+    if not args.input:
+        args.input = [x for x in os.listdir() if x.endswith('nr.blast')][0]
+    nr(args.input)
+    print('\nDone!\n')
 
 
-
-
-
-
-
-
-
-
-# dic_nr={}
-# for each_doc in dir_list:
-#     if ('nr_diamond.blast' in each_doc):
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# input_doc='Trifolium_repens_Baisanye_unigene_CDS_nr_diamond.blast'
-# f1=open(input_doc,'r')
-# f2=open(input_doc.split('.blast')[0]+'_extract.txt','w')
-# for each_line in f1:
-#     gene_id=each_line.split('\t')[0]
-#     annotation=each_line.strip().split('\t')[-1]
-#     if gene_id in dic_nr:
-#         dic_nr[gene_id]+=';'+annotation
-#     else:
-#         dic_nr[gene_id]=annotation
-# for each_key in dic_nr:
-#     f2.write(each_key+'\t'+dic_nr[each_key]+'\n')
-#
-#
-#
-# f1.close()
-#
-# f2.close()
+if __name__ == '__main__':
+    main()
 
