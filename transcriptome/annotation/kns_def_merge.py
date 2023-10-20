b39d736ac0a38a2cca1f5c2c829d54d958eaf1a1
WilliamGoGo <realwilliamchin@outlook.com>
2023-10-20 13:03:30 +0800

    [BUG FIXED] transcriptome/annotation/kns_def_merge.py

--- transcriptome/annotation/kns_def_merge.py
+++ transcriptome/annotation/kns_def_merge.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Created Time  : 2023/08/18 09:22
 # Author        : William GoGo
+import os
 import argparse
 import pandas as pd
 from genedf_add_def import add_kns_def
@@ -12,8 +13,8 @@ def parse_input():
     argparser.add_argument('-k', '--kegg', help='KEGG_gene_def 文件，会添加 KEGG_ID 和 KEGG_Shortname 列')
     argparser.add_argument('-n', '--nr', help='NR_gene_def 文件')
     argparser.add_argument('-s', '--swiss', help='Swiss_gene_def 文件')
-    argparser.add_argument('-i', '--input', help='输入 all_geneid 文件')
-    argparser.add_argument('-o', '--output', default='_kns_def.txt', help='输出文件')
+    argparser.add_argument('-i', '--input', required=True, help='输入 all_geneid 文件')
+    argparser.add_argument('-o', '--output', help='输出文件')
     return argparser.parse_args()
 
 
@@ -23,9 +24,8 @@ def main():
     df = pd.read_csv(args.input, sep='\t', names=['GeneID'], dtype={"GeneID": str})
     result_df = add_kns_def(df, args.kegg, args.nr, args.swiss)
     
-    if not args.output and args.input:
-        args.output = args.input.split('_all')[0]
-    
+    if not args.output:
+        args.output = args.input.split(os.sep)[-1].split('_all')[0] + '_kns_def.txt'
     result_df.to_csv(args.output, sep='\t', index=False)
 
 
