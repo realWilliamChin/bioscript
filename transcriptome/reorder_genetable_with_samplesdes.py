56e9ec96a1351c6414f89cb925f048523cb0cb68
WilliamGoGo <realwilliamchin@outlook.com>
2023-11-16 11:17:30 +0800

    [REFACOTR] transcriptome/reorder_genetable_with_samplesdes.py 把替换文件更新为选择输出文件名

--- transcriptome/reorder_genetable_with_samplesdes.py
+++ transcriptome/reorder_genetable_with_samplesdes.py
@@ -11,7 +11,7 @@ def parse_input():
     parser.add_argument('-s', '--sample', default='samples_described.txt',
                         help='指定 samples_described.txt 文件, 默认当前文件夹下的 samples_described.txt')
     parser.add_argument('-f', '--file', required=True, help='输入需要重新对列排序的文件')
-    parser.add_argument('-r', '--isreplace', action='store_true', help='是否对源文件替换，不替换则会重新生成 _realign.txt')
+    parser.add_argument('-o', '--output', default='output.txt', help='生成的文件名，默认在当前文件夹下生成 output.txt')
     args = parser.parse_args()
     return args
 
@@ -28,7 +28,7 @@ def check_columns_name(lst, df_columns):
         print(message)
     
     
-def reindex(lst, file, replace_or_not):
+def reindex(lst, file, output_file):
     df = pd.read_csv(file, sep='\t', dtype=str)
     lst.insert(0, df.columns[0])
     df = df.reindex(columns=lst)
@@ -39,10 +39,7 @@ def reindex(lst, file, replace_or_not):
         print('输入的 title list 和输出的表 title 数量不匹配，请检查')
         exit(1)
 
-    if replace_or_not:
-        df.to_csv(file, sep='\t', index=False)
-    else:
-        df.to_csv(file.replace('.txt', '_realign.txt'), sep='\t', index=False)
+    df.to_csv(output_file, sep='\t', index=False)
 
 
 def main():
@@ -50,7 +47,7 @@ def main():
     
     sample_arr = list(pd.read_csv(args.sample, sep='\t')['sample'])
     check_columns_name(sample_arr, pd.read_csv(args.file, sep='\t').columns.values)
-    reindex(sample_arr, args.file, args.isreplace)
+    reindex(sample_arr, args.file, args.output)
 
     print('\nDone!\n')
 
