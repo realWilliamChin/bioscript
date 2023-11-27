2b24fe518e2cc0eacc4196601ed90fdd64b8a5ab
WilliamGoGo <realwilliamchin@outlook.com>
2023-11-27 14:01:52 +0800

    [NEW FEATURE] Rscript/WGCNA/wgcna_pll_v5.r 添加 color_module 目录，All_Gene_KME 每个颜色分成一个 txt 到 color_module 目录

--- Rscript/WGCNA/wgcna_pll_v5.r
+++ Rscript/WGCNA/wgcna_pll_v5.r
@@ -1,10 +1,10 @@
-setwd("L:/work/01_Refenrence/2023_10_16_xiaomai/2023_11_16_WGCNA/WGCNA_1")
+setwd("L:/work/01_Refenrence/2023_10_16_xiaomai/2023_11_16_WGCNA/WGCNA_7")
 library(DESeq2)
 library(WGCNA)
 library(genefilter)
 library(tidyr)
 library(tidyverse)
-allowWGCNAThreads(32)
+allowWGCNAThreads(10)
 options(stringsAsFactors = FALSE)
 getwd()
 rm(list=ls())
@@ -177,7 +177,7 @@ dev.off()
 sft$powerEstimate
 #如果上一步最佳估计的结果是[1] NA，自己根据绘制的图，选择一个最佳的power，选择wgcna-02-1.pdf右边图里下降曲线变平滑的那个值
 #这里一个具体的例子，选了10.但是不同的数据，肯定是不同的。
-sft$powerEstimate<-10
+sft$powerEstimate<-8
 #一步法构建共表达矩阵,注意这个power值的选择，此power值应选择wgcna-02-1.pdf右边图里下降曲线变平滑处的值
 net = blockwiseModules(datExpr, power = sft$powerEstimate, TOMType = "unsigned", minModuleSize = 30, reassignThreshold =0, mergeCutHeight = 0.25, numericLabels = TRUE, pamRespectsDendro = FALSE, saveTOMs = TRUE,  saveTOMFileBase = "fpkmTOM", verbose = 3)
 table(net$colors)
@@ -253,11 +253,11 @@ METree = hclust(as.dist(MEDiss), method = "average");
 #sizeGrWindow(7, 6)
 #png("Clustering_of_module_eigengenes1.png",width = 800,height = 600)
 
-png("Analysis_results/Clustering_of_module_eigengenes1_0.21.png",width = 800,height = 600)
+png("Analysis_results/Clustering_of_module_eigengenes1_0.37.png",width = 800,height = 600)
 plot(METree, main = "Clustering of module eigengenes",     xlab = "", sub = "")
 #######################这个地方需要手动选择理想的cutoff,这个值越大，模块数越少##################################
 ##################MEDissThres值的选择根据Clustering_of_module_eigengenes1.png图来决定，值越大，模块数越少
-MEDissThres = 0.21
+MEDissThres = 0.37
 
 # Plot the cut line into the dendrogram
 abline(h=MEDissThres, col = "red")
@@ -267,7 +267,7 @@ dev.off()
 
 
 # Call an automatic merging function MEDissThres=0.1
-MEDissThres = 0.21
+MEDissThres = 0.37
 merge = mergeCloseModules(datExpr, dynamicColors, cutHeight = MEDissThres, verbose = 3)
 str(merge)
 # The merged module colors
@@ -423,4 +423,13 @@ for (sub_edge_file in edge_files) {
 }
 #########################################################################################################
 #head(node_freq)
+dir.create("Color_Module")
+all_gene_kme<-read.table("All_Gene_KME.txt",sep="\t",header=F,stringsAsFactors = F,check.names = F,quote='')
+colors <- unique(all_gene_kme$V3)
+for (each_color in colors) {
+  color_module_file_name = paste0("Color_Module/gene_", each_color, '.txt')
+  color_module_tmp <- all_gene_kme[all_gene_kme$V3==each_color,]
+  write.table(color_module_tmp,file = color_module_file_name,row.names=F,quote=F,col.names=F,sep="\t")
+  
+}
 
