130c2b11f964c24abe213df83e8d8db949946c9a
WilliamGoGo <realwilliamchin@outlook.com>
2023-12-12 17:32:42 +0800

    [REFACTOR] transcriptome/reference/hisat2_mapping_summary.py 添加输入 samples_described.txt 按照 samples 的顺序输出 mapping_summary，整个代码重构

--- transcriptome/reference/hisat2_mapping_summary.py
+++ transcriptome/reference/hisat2_mapping_summary.py
@@ -1,43 +1,55 @@
 #!/home/train/miniconda3/bin/python
 # -*- coding: utf-8 -*-
 # Created Time  : 2023/06/20 21:45
-# Author        : chenshen
+# Author        : William GoGo, chenshen
 import os
+import argparse
 
-out_doc = 'mapping_summary.txt'
-f1=open(out_doc,'w')
-f1.write('Sample'+'\t'+'Total reads'+'\t'+'Mapped reads'+'\t''Unmapped reads'+'\t'+'Unique mapped reads'+'\t'+'Multiple mapped reads'+'\t'+'overall alignment rate'+'\n')
 
-for dir_name in os.listdir():
-    if '_mapping.txt' in dir_name:
-        sample=dir_name.split('_mapping.txt')[0]
-        f1.write(sample+'\t')
-        with open(dir_name) as f2:
-            data_list=[]
-            row_num = 0
-            for line in f2:
-                if 'nohup' in line:
-                    continue
-                line=line.strip()
-                '''
-                if row_num==0:
-                    total_reads=line.split(' ')[0]
-                    f1.write(total_reads+'\t')
-                if row_num==3:
-                    mapped_reads=line.split(' ')[0]
-                    f1.write(total_reads+'\t')
-                row_num+=1
-                '''
-                data_list.append(line.split(' ')[0])
+def parse_input():
+    args = argparse.ArgumentParser(description='hisat2_mapping_summary.py')
+    args.add_argument('-i', '--input', help='input hisat2 mapping dir', default='.')
+    args.add_argument('-o', '--output', help='output mapping summary file', default='mapping_summary.txt')
+    args.add_argument('-s', '--samples', help='samples_described.txt', default='samples_described.txt')
 
-            total_reads=int(data_list[0])
-            mapped_reads=int(int(data_list[3])+int(data_list[4])+int(data_list[7])+(int(data_list[12])+int(data_list[13]))/2)
-            unmapped_reads=total_reads-mapped_reads
-            unique_mapped=int(data_list[3])+int(data_list[7])
-            multi_mapped_reads=mapped_reads-unique_mapped
-            overall_rate = data_list[14].split(' ')[0]
-            f1.write(str(total_reads)+'\t'+str(mapped_reads)+'\t'+str(unmapped_reads)+'\t'+str(unique_mapped)+'\t'+str(multi_mapped_reads)+'\t'+str(overall_rate)+'\n')
-f1.close()
+    return args.parse_args()
+
+
+def process_mapping(mapping_file):
+    with open(mapping_file) as f2:
+        data_list=[]
+        for line in f2:
+            if 'nohup' in line:
+                continue
+            line=line.strip()
+            data_list.append(line.split(' ')[0])
+
+        total_reads=int(data_list[0])
+        mapped_reads=int(int(data_list[3])+int(data_list[4])+int(data_list[7])+(int(data_list[12])+int(data_list[13]))/2)
+        unmapped_reads=total_reads-mapped_reads
+        unique_mapped=int(data_list[3])+int(data_list[7])
+        multi_mapped_reads=mapped_reads-unique_mapped
+        overall_rate = data_list[14].split(' ')[0]
+    return total_reads, mapped_reads, unmapped_reads, unique_mapped, multi_mapped_reads, overall_rate
+
+
+def main():
+    args = parse_input()
+    samples_data = open(args.samples, 'r').readlines()
+    sample_data_list = [os.sep.join([args.input, x.split('\t')[1] + '_mapping.txt']) for x in samples_data[1:] if x.strip() != '']
+    
+    column_name = ['Sample', 'Total reads', 'Mapped reads', 'Unmapped reads', 'Unique mapped reads', 'Multiple mapped reads', 'overall alignment rate']
+    open(args.output, 'w').write('\t'.join(column_name)+'\n')
+    
+    for sample_data in sample_data_list:
+        sample_name = os.path.basename(sample_data).replace('_mapping.txt', '')
+        total_reads, mapped_reads, unmapped_reads, unique_mapped, multi_mapped_reads, overall_rate = process_mapping(sample_data)
+        open(args.output, 'a').write(f'{sample_name}\t{total_reads}\t{mapped_reads}\t{unmapped_reads}\t{unique_mapped}\t{multi_mapped_reads}\t{overall_rate}\n')
+    
+
+
+if __name__ == '__main__':
+    main()
 
 
 
