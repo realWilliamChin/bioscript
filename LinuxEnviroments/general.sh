002e19d591fc4ec907bedbaecc12affba1fb5938
WilliamGoGo <realwilliamchin@outlook.com>
2024-04-10 15:49:25 +0800

    [NEW FILE] LinuxEnviroments/general.sh 系统软件环境变量

---
+++ LinuxEnviroments/general.sh
@@ -0,0 +1,190 @@
+PATH=$PATH:/home/train/.aspera/connect/bin/
+PATH=$PATH:/opt/biosoft/sratoolkit/bin/
+PATH=$PATH:/opt/biosoft/FastQC/
+PATH=$PATH:/opt/biosoft/FastUniq/source/
+PATH=$PATH:/opt/biosoft/BLESS/
+alias bless='mkdir -p kmc/bin/; cp /opt/biosoft/BLESS/kmc/bin/kmc kmc/bin/; bless'
+PATH=$PATH:/opt/biosoft/ALLPATHS-LG/bin/
+PATH=/opt/sysoft/cmake-3.4.1/bin/:$PATH
+
+PATH=$PATH:/opt/biosoft/FastUniq/source/
+PATH=$PATH:/opt/biosoft/BLESS/
+PATH=$PATH:/opt/biosoft/ALLPATHS-LG/bin/
+
+PATH=$PATH:/opt/biosoft/lordec-src_0.9/
+PATH=$PATH:/opt/biosoft/idba-1.1.3/bin/
+PATH=$PATH:/opt/biosoft/SOAPdenovo2-r241/
+PATH=$PATH:/opt/biosoft/ALLPATHS-LG/bin/
+PATH=$PATH:/opt/biosoft/DBG2OLC
+PATH=$PATH:/opt/biosoft/wtdbg-2.4_x64_linux/
+PATH=$PATH:/opt/biosoft/minimap2-2.17_x64-linux/
+PATH=$PATH:/opt/biosoft/Platanus_allee_v2.0.2_Linux_x86_64
+PATH=$PATH:/opt/biosoft/quickmerge-0.3
+PATH=$PATH:/opt/biosoft/mummer-4.0.0beta2/bin/
+PATH=$PATH:/opt/biosoft/GapFiller_v1-11_linux-x86_64/
+PATH=$PATH:/opt/biosoft/GapCloser-v1.12-r6/
+PATH=$PATH:/opt/biosoft/SSPACE-STANDARD-3.0_linux-x86_64/
+PATH=$PATH:/opt/biosoft/bowtie2-2.3.5-linux-x86_64/
+
+PATH=$PATH:/opt/biosoft/ncbi-blast-2.6.0+/bin/
+PATH=$PATH:/opt/biosoft/RepeatMasker
+PATH=$PATH:/opt/biosoft/RepeatModeler-open-1.0.11
+PATH=$PATH:/opt/biosoft/infernal-1.1.2/bin
+PATH=$PATH:/opt/biosoft/Misa_Primer3/
+PATH=$PATH:/opt/biosoft/primer3-2.4.0/src/
+PATH=$PATH:/opt/biosoft/tRNAscan-SE-2.0/bin/
+PATH=$PATH:/opt/biosoft/tRNAscan-SE-1.3.1/bin/
+export PERL5LIB=$PERL5LIB:/opt/biosoft/tRNAscan-SE-1.3.1/bin/
+PATH=$PATH:/opt/biosoft/bowtie-1.2.2-linux-x86_64/
+PATH=$PATH:/opt/biosoft/bwa-0.7.17/
+PATH=$PATH:/opt/biosoft/samtools-1.9/bin/
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/biosoft/htslib-1.9/lib/
+C_INCLUDE_PATH=$C_INCLUDE_PATH:/opt/biosoft/htslib-1.9/include/
+PATH=$PATH:/opt/biosoft/htslib-1.9/bin/
+PATH=$PATH:/opt/biosoft/tophat-2.1.1.Linux_x86_64/
+PATH=$PATH:/opt/biosoft/bcftools-1.9/bin
+PATH=$PATH:/opt/biosoft/hisat2-2.1.0/
+# GATK4.1.2 对 MNPs 支持不太好，修改为 4.4.0.0 版本 - 2023_09_21
+# PATH=$PATH:/opt/biosoft/gatk-4.1.2.0/
+PATH=$PATH:/opt/biosoft/gatk-4.4.0.0/
+PATH=$PATH:/opt/biosoft/Trinity-v2.8.5/
+#export PATH=/opt/biosoft/salmon-1.9.0_linux_x86_64/salmon-1.9.0_linux_x86_64/bin:$PATH
+#PATH=$PATH:/opt/biosoft/salmon-latest_linux_x86_64/bin/
+PATH=$PATH:/opt/biosoft/RSEM-1.3.1/
+PATH=/opt/sysoft/cmake-3.14.0/bin/:$PATH
+PATH=$PATH:/opt/biosoft/kallisto
+PATH=$PATH:/opt/biosoft/TransDecoder-v5.5.0/
+PATH=/opt/biosoft/hmmer-3.2.1/bin/:$PATH
+PATH=$PATH:/opt/biosoft/cufflinks-2.2.1.Linux_x86_64/
+PATH=$PATH:/opt/biosoft/stringtie-1.3.5.Linux_x86_64/
+PATH=$PATH:/opt/biosoft/blat/
+PATH=$PATH:/opt/biosoft/fasta-36.3.8g/bin/
+PATH=$PATH:/opt/biosoft/PASApipeline-v2.3.3/bin/
+PATH=$PATH:/opt/biosoft/gmap-2017-11-15/bin/
+PATH=$PATH:/opt/biosoft/blast-2.2.26/bin/
+PATH=$PATH:/opt/sysoft/lighttpd-1.4.54/sbin/
+PATH=$PATH:/opt/biosoft/wise2.4.1/src/bin/
+export WISECONFIGDIR=/opt/biosoft/wise2.4.1/wisecfg/
+PATH=/opt/biosoft/hmmer-3.2.1/bin/:$PATH
+PATH=$PATH:/opt/biosoft/blat/
+PATH=$PATH:/opt/biosoft/fasta-36.3.8g/bin/
+PATH=$PATH:/opt/biosoft/PASApipeline-v2.3.3/bin/
+PATH=$PATH:/opt/biosoft/blast-2.2.26/bin/
+PATH=$PATH:/opt/biosoft/gmap-2017-11-15/bin/
+PATH=$PATH:/opt/sysoft/lighttpd-1.4.54/sbin/
+PATH=$PATH:/opt/biosoft/augustus-3.3.2/bin/
+PATH=$PATH:/opt/biosoft/augustus-3.3.2/scripts/
+export AUGUSTUS_CONFIG_PATH=/opt/biosoft/augustus-3.3.2/config/
+PATH=/opt/biosoft/hmmer-3.2.1/bin/:$PATH
+PATH=$PATH:/opt/biosoft/gmes_linux_64/
+PATH=$PATH:/opt/biosoft/exonerate-2.2.0-x86_64/bin/
+PATH=$PATH:/opt/biosoft/snap
+PATH=$PATH:/opt/biosoft/BRAKER-2.1.2/scripts/
+PATH=$PATH:/opt/biosoft/geta-2.4.3/bin/
+PATH=$PATH:/opt/biosoft/SpliceGrapher-0.2.7/scripts/
+PATH=$PATH:/opt/biosoft/isolasso/bin/
+PATH=$PATH:/opt/biosoft/gms2_linux_64//
+PATH=$PATH:/opt/biosoft/sequin/
+PATH=$PATH:/opt/biosoft/tbl2asn/
+PATH=$PATH:/opt/biosoft/IGV_Linux_2.5.3/
+PATH=$PATH:/opt/biosoft/circos-0.69-6/bin/
+PATH=/opt/biosoft/ncbi-blast-2.9.0+/bin/:$PATH
+PATH=$PATH:/opt/biosoft/diamond
+PATH=$PATH:/opt/biosoft/mcl-14-137/bin/
+PATH=$PATH:/opt/biosoft/orthomclSoftware-v2.0.9/bin/
+PATH=$PATH:/opt/biosoft/mafft/bin/
+PATH=$PATH:/opt/biosoft/samtools-1.9/bin/
+PATH=$PATH:/opt/biosoft/Gblocks_0.91b/
+export PROTTEST_HOME=/opt/biosoft/prottest-3.4.2
+PATH=$PATH:/opt/biosoft/RAxML-8.2.12/
+PATH=$PATH:/opt/biosoft/r8s1.81/src
+PATH=$PATH:/opt/biosoft/beast/bin/
+PATH=$PATH:/opt/biosoft/Tracer_v1.7.1/bin/
+PATH=$PATH:/opt/biosoft/CAFE-4.2.1/release/
+PATH=$PATH:/opt/biosoft/MCScanX/
+export PKG_CONFIG_PATH=/opt/biosoft/beagle-lib-3.1.2/lib/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=/opt/biosoft/beagle-lib-3.1.2/lib/:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=/opt/biosoft/beagle-lib-3.1.2/include:$C_INCLUDE_PATH
+PATH=$PATH:/opt/biosoft/mauve_2.4.0/
+PATH=$PATH:/opt/biosoft/mummer-4.0.0beta2/bin/
+PATH=$PATH:/opt/biosoft/SPAdes-3.13.0-Linux/bin/
+export PATH=/opt/biosoft/GetOrganelle-master/:$PATH
+export PATH=/opt/biosoft/GetOrganelle-master/Utilities/:$PATH
+export PATH=$PATH:/opt/biosoft/python27/bin/
+export PATH=/opt/biosoft/PGA:$PATH
+export PATH=/opt/biosoft/fastp/:$PATH
+export PATH=/opt/biosoft/sickle/:$PATH
+export PATH=$PATH:/opt/biosoft/canu-1.8/Linux-amd64/bin/
+export PATH=$PATH:$HOME/local/app/edirect
+PATH=$PATH:/home/train/smrtlink/smrtcmds/bin/
+#export PATH=/home/train/software/ProtHint/bin/:$PATH
+PATH=$PATH:/opt/biosoft/maker/bin/
+PATH=$PATH:/opt/biosoft/exonerate-2.2.0-x86_64/bin/
+PATH=$PATH:/opt/biosoft/samtools-1.9/bin/
+PATH=$PATH://home/data/opt/biosoft/subread-2.0.1-Linux-x86_64/bin/
+PATH=$PATH:/home/train/software/ProtHint/bin
+export PATH=/opt/biosoft/taxonkit:$PATH
+export PATH=/opt/biosoft/csvtk:$PATH
+export PATH=/opt/biosoft/FLASH-1.2.11-Linux-x86_64:$PATH
+export PATH=/opt/biosoft/bioawk:$PATH
+export PATH=/opt/biosoft/MCScanX/downstream_analyses:$PATH
+export PATH=/opt/biosoft/MCScanX/downstream_analyses/:$PATH
+export PATH=/opt/biosoft/MetaGeneMark_linux_64/mgm:$PATH
+export PATH=/home/data/opt/biosoft/centrifuge:$PATH
+export PATH=/opt/biosoft/MEGAHIT-1.2.9-Linux-x86_64-static/bin:$PATH
+export PATH=/opt/biosoft/cdhit-4.6.2:$PATH
+export PATH=/opt/biosoft/bpipe-0.9.9.9/bin:$PATH
+export PATH=/opt/biosoft/R40/bin:$PATH
+export PATH=/opt/biosoft/seqtk:$PATH
+export PATH=/opt/biosoft/seqkit:$PATH
+export PATH=/opt/biosoft/RpsbProc-x64-linux:$PATH
+export PATH=/opt/biosoft/cutadapt:$PATH
+PATH=/opt/biosoft/rnammer-1.2:$PATH
+
+export PATH=/home/train/miniconda3/envs/test/bin:$PATH
+export PATH=/home/train/miniconda3/envs/test2/bin:$PATH
+export PATH=/opt/biosoft/TrimGalore-0.6.0:$PATH
+export PATH=/opt/biosoft/salmon-1.9.0_linux_x86_64/salmon-1.9.0_linux_x86_64/bin:$PATH
+
+
+# qinqiang
+#export JAVA_HOME=/opt/biosoft/openjdk17/jdk-17.0.2
+#export PATH=$JAVA_HOME/bin:$PATH
+export CPC_HOME=/opt/biosoft/CPC2-beta
+export PATH=$PATH:/opt/biosoft/vcftools-0.1.16/src/cpp
+export PATH=$PATH:/opt/biosoft/plink_1.9
+export PATH=$PATH:/opt/biosoft/admixture_linux-1.3.0
+export PATH=$PATH:/opt/biosoft/eggnog-mapper-1.0.3
+
+# qinqiang python script
+export ScriptPATH=/home/colddata/qinqiang/script/
+export NoRefScriptPATH=/home/colddata/qinqiang/ProjectScript/01_NoReference/
+export RefScriptPATH=/home/colddata/qinqiang/ProjectScript/02_Reference/
+export PATH=$PATH:$ScriptPATH:$NoRefScriptPATH:$RefScriptPATH
+
+
+# Augustus-3.4.0
+export AUGUSTUS_CONFIG_PATH=/opt/biosoft/augustus-3.4.0/config
+export AUGUSTUS_BIN_PATH=/opt/biosoft/augustus-3.4.0/bin
+export AUGUSTUS_SCRIPTS_PATH=/opt/biosoft/augustus-3.4.0/scripts
+
+# braker3
+export BAMTOOLS_PATH=/opt/biosoft/bamtools-2.5.1/src/usr/local/bin
+export TOOLDIR=/opt/biosoft/samtools-1.17
+#export PROTHINT_PATH=/opt/biosoft/ProtHint-2.6.0/bin
+export TSEBRA_PATH=/opt/biosoft/TSEBRA-1.1.0/bin
+#export GENEMARK_PATH=/opt/biosoft/GeneMark-ETP/bin
+export GENEMARK_PATH=/opt/biosoft/gmes_linux_64_4/bin
+export PROTHINT_PATH=/opt/biosoft/gmes_linux_64_4/ProtHint/bin
+export PATH=$PATH:/opt/biosoft/BRAKER-3.0.3/scripts
+
+# qinqiang.sh
+export ScriptPATH=/home/colddata/qinqiang/script/
+export NoRefScriptPATH=/home/colddata/qinqiang/ProjectScript/01_NoReference/
+export RefScriptPATH=/home/colddata/qinqiang/ProjectScript/02_Reference/
+export PATH=$PATH:$ScriptPATH:$NoRefScriptPATH:$RefScriptPATH
+
+# R-4.2.2.sh
+export R_HOME=/opt/biosoft/R-4.2.2
+export PATH=$R_HOME/bin:$PATH
+export R_LIBS=/opt/biosoft/R-4.2.2/lib64/R/library
