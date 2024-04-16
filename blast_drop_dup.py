8369299c976db22188678bc9074e302422e91a2e
WilliamGoGo <realwilliamchin@outlook.com>
2024-04-16 15:07:12 +0800

    [PERF] blast_drop_dup.py 对不同 blast 文件的列名数量不一样优化，只对第一列和倒数第二列id和score进行去重

--- blast_drop_dup.py
+++ blast_drop_dup.py
@@ -2,27 +2,23 @@
 # -*- coding: utf-8 -*-
 # Created Time  : 2023/10/17 17:47
 # Author        : William GoGo
-
+import sys
 import argparse
 import pandas as pd
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='biogrid')
+    parser = argparse.ArgumentParser(description='按照 qacc 第一列和倒数第二列 bitscore 的得分进行去重')
     parser.add_argument('-i', '--infile', required=True, help='输入 blast 文件')
     parser.add_argument('-o', '--outfile', help='输出 uniq blast 的文件名')
+    parser.add_argument('-n', '--column-names', help='输出 blast 列名 格式这么写，qacc,sacc,pident', dest='column_names',
+                        default="qacc sacc pident qcovs qcovhsp ppos length mismatch gapopen qstart qend sstart send evalue bitscore stitle")
+    
+    args = parser.parse_args()
+    args.column_names = args.column_names.split()
+
     return parser.parse_args()
 
-
-def drop_dup(blast_file, out_file):
-    blast_names = ['qacc','sacc','qcovhsp','ppos','length','mismatch','gapopen','qstart','qend','sstart','send','evalue','bitscore','stitle']
-    # df = pd.read_csv(blast_file, sep='\t', names=blast_names)
-    df = pd.read_csv(blast_file, sep='\t', header=None)
-    print(df.columns.values)
-    df.sort_values(by=[0, 12], ascending=[True, False], inplace=True)
-    df.drop_duplicates(subset=0, keep='first', inplace=True)
-    df.to_csv(out_file, sep='\t', index=False, header=blast_names)
-    
 
 def main():
     args = parse_arguments()
@@ -30,8 +26,18 @@ def main():
         outfile = args.outfile
     else:
         outfile = str(args.infile).replace('.blast', '_uniq.blast')
-    drop_dup(args.infile, outfile)
     
+    df = pd.read_csv(args.infile, sep='\t', header=None)
+    print(df.columns.values)
+    # 计算倒数第二列的正整数索引
+    index_last_second = df.shape[1] - 2  # 列总数减2得到倒数第二列的索引
+    # 按第一列升序和倒数第二列降序排序
+    df.sort_values(by=[df.columns[0], df.columns[index_last_second]], ascending=[True, False], inplace=True)
+    df.drop_duplicates(subset=[0], keep='first', inplace=True)
+    if args.column_names:
+        df.to_csv(outfile, sep='\t', index=False, header=args.column_names)
+    else:
+        df.to_csv(outfile, sep='\t', index=False, header=None)
 
 if __name__ == '__main__':
     main()
