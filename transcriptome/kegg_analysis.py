057a38e1f23e05518a4c7dce3f3ba676828f64fc
WilliamGoGo <realwilliamchin@outlook.com>
2024-05-10 17:58:41 +0800

    [PERF] transcriptome/kegg_analysis.py 添加日志loguru函数，输出更多当前进度内容，对错误进行输出

--- transcriptome/kegg_analysis.py
+++ transcriptome/kegg_analysis.py
@@ -7,11 +7,24 @@ import sys
 import pandas as pd
 import argparse
 import subprocess
+from loguru import logger
 
 sys.path.append(os.path.abspath('/home/colddata/qinqiang/script/CommonTools/'))
 
 
 def kid_optmial(input_df):
+    """根据输入的 MultiDESeq 结果文件 DEG DataFrame，计算 up down 数量的选择
+    up > down = 1
+    up < down = -1
+    up = down = 0
+
+    Args:
+        input_df (DataFrame): DEG 的每个 Dataframe
+
+    Returns:
+        dict: 包含每个孩子最佳选择的字典，键为孩子ID，值为最佳选择
+        1表示上升趋势，-1表示下降趋势，0表示无明显趋势。
+    """
     id_list = input_df.to_dict(orient='split')['data']
     dic = {}
     result_dic = {}
@@ -40,15 +53,21 @@ def kid_optmial(input_df):
 
 
 def up_down_idlist_geneid2kid(down_df, up_df, output_prefix, kegg_clean):
-    """
-    可以读取 de_results_add_def.py 生成的 DEG_data.txt 文件
+    """可以读取 de_results_add_def.py 生成的 DEG_data.txt 文件
     把 regulation 变为 1,0,-1 对应 up,nosig,down
     生成的文件只包括 K_ID 和 regulation
     例如: K12345    0
     一个 K_ID 对应多个 GeneID, 有可能 up,down,nosig 都有
     对 K_ID 去重，哪个 regulation 多留下哪个，一样多的变为 0
     并生成一个中间文件，方便检查
+
+    Args:
+        down_df (DataFrame): down id list 列名为 GeneID
+        up_df (_type_): up id list 列名为 GeneID
+        output_prefix (_type_): 输出前缀
+        kegg_clean (_type_): KEGG_clean 文件
     """
+
     kegg_clean_df = pd.read_csv(kegg_clean, sep='\t', usecols=[0, 4], names=['GeneID', 'KEGG_ID'], dtype=str)
     
     down_df = pd.merge(left=down_df, right=kegg_clean_df, on='GeneID', how='left')
@@ -102,14 +121,21 @@ def other(args):
     ko_gene_df = ko_gene_df[output_columns_list]
     ko_gene_df.to_csv(output_prefix + '_ko_geneid_def.txt', sep='\t', index=False)
     
-    
     ko_gene_df['regulation'] = 1
     ko_gene_df[['KEGG_ID', 'regulation']].to_csv(output_prefix + '_keggid_regulation.txt', sep='\t', index=False)
 
 
 def transcriptome_all_ko_gene_heatmap(ko_file, kegg_clean_file, fpkm_matrix_file, samples_file):
+    """针对一些 KEGG_ID 的相关基因画出一个 heatmap 图
+
+    Args:
+        ko_file (str): 张老师给的 KEGG_ID 文件
+        kegg_clean_file (str): kegg 注释出来的 KEGG_clean.txt
+        fpkm_matrix_file (str): fpkm 矩阵文件
+        samples_file (str): 样本描述文件，通常为 samples_described.txt 
+    """
     # 对 expression fpkm matrix 文件只保留 fpkm 值
-    expression_df = pd.read_csv(fpkm_matrix_file, sep='\t', dtype=str)
+    expression_df = pd.read_csv(fpkm_matrix_file, sep='\t', dtype={'GeneID': str})
     expression_df = expression_df.drop_duplicates(subset='GeneID')
     expression_df_columns = expression_df.columns.tolist()
     expression_df_columns = [expression_df_columns[0]] + [x for x in expression_df_columns if x.endswith("_fpkm")]
@@ -160,6 +186,11 @@ def transcriptome_all_ko_gene_heatmap(ko_file, kegg_clean_file, fpkm_matrix_file
     
 
 def transcriptome(args):
+    """针对转录组的 KEGG 分析
+
+    Args:
+        args (_type_): 所有的输入参数
+    """
     # 循环每个 ko number 筛选表达量画热图
     ko_df = pd.read_csv(args.ko_list, sep='\t')
     # 针对两种情况，一种只有 kolist，还有一种有 Ontology，就需要画另一个图了
@@ -167,7 +198,11 @@ def transcriptome(args):
         ko_list = ko_df['KEGG_ID'].values.tolist()
         transcriptome_all_ko_gene_heatmap(args.ko_list, args.kegg_clean, args.expression, args.samplesinfo)
     else:
-        ko_list = open(args.ko_list, 'r').read().strip().split('\n')
+        logger.warning('检测到 Ontology 列不在文件中，将只读取第一列 KEGG_ID 进行分析，不画 all ko gene heatmap 图')
+        ko_list = pd.read_csv(args.ko_list, sep='\t', usecols=[0]).values.tolist()
+        if 'KEGG_ID'.lower() in ko_list:
+            ko_list = ko_list[1:]
+        # ko_list = open(args.ko_list, 'r').read().strip().split('\n')
     
     deg_data_list = os.listdir(args.deg_data_dir)
     kegg_pathway_df = pd.read_csv(args.kegg_clean, sep='\t', names=['GeneID', 'Ko'], usecols=[0, 1], dtype=str)
@@ -182,7 +217,7 @@ def transcriptome(args):
         control_group = deg_data_df.iloc[0, 2]
         compare_info = treat_group + '_vs_' + control_group
         
-        print(f"\n====正在处理 {compare_info}====")
+        logger.info(f"====正在处理 {compare_info}====")
         up_df = deg_data_df[deg_data_df['regulation'] == 'Up']['GeneID']
         down_df = deg_data_df[deg_data_df['regulation'] == 'Down']['GeneID']
         
@@ -193,7 +228,7 @@ def transcriptome(args):
         
         treat_group_samples_name = samples_described_df[samples_described_df['group'].isin([treat_group,])]['sample'].to_list()
         control_group_samples_name = samples_described_df[samples_described_df['group'].isin([control_group,])]['sample'].to_list()
-        print(f"crt_group_samples: {crt_group_samples}")
+        logger.debug(f"crt_group_samples: {crt_group_samples}")
         
         heatmap_sheet2_df = pd.DataFrame(columns=['samples', 'group', 'colors'])
         for each_sample in crt_group_samples:
@@ -203,13 +238,16 @@ def transcriptome(args):
             else:
                 group = control_group
                 color = args.control_color
-            heatmap_sheet2_df = heatmap_sheet2_df.append({'samples': each_sample, 'group': group, 'colors': color}, ignore_index=True)
+            row_data = {'samples': each_sample, 'group': group, 'colors': color}
+            row_df = pd.DataFrame([row_data])
+            heatmap_sheet2_df = pd.concat([heatmap_sheet2_df, row_df], ignore_index=True)
+            # heatmap_sheet2_df = heatmap_sheet2_df.append({'samples': each_sample, 'group': group, 'colors': color}, ignore_index=True)
 
         for ko_number in ko_list:
             ko_num_fpkm_expr_df = fpkm_df[fpkm_df['GeneID'].isin(kegg_pathway_df[kegg_pathway_df['Ko'].str.contains(ko_number)]['GeneID'])]
             
             if ko_num_fpkm_expr_df.shape[0] == 0:
-                print(f"{ko_number} 在 {compare_info} 中相关的基因表达量表为空")
+                logger.warning(f"{ko_number} 在 {compare_info} 中相关的基因表达量表为空")
                 continue
             
             ko_num_fpkm_expr_df_file = f"{compare_info}_{ko_number}_fpkm_expression.xlsx"
@@ -218,10 +256,10 @@ def transcriptome(args):
                 ko_num_fpkm_expr_df.to_excel(writer, index=False, sheet_name='Sheet1')
                 heatmap_sheet2_df.to_excel(writer, index=False, sheet_name='Sheet2')
             if ko_num_fpkm_expr_df.shape[0] > 1:
-                print(f"正在画 {compare_info} {ko_number} 相关基因表达量的热图")
+                logger.info(f"正在画 {compare_info} {ko_number} 相关基因表达量的热图")
                 draw_twogroup_heatmap(ko_num_fpkm_expr_df_file, ko_num_fpkm_expr_pic_name)
             else:
-                print(f"{compare_info} 的 {ko_number} 中相关的基因表达量表只有一行，无法画热图")
+                logger.warning(f"{compare_info} 的 {ko_number} 中相关的基因表达量表只有一行，无法画热图")
             
             # 输出每个 ko 的 deg_data 文件
             ko_num_deg_data_file = f"{compare_info}_{ko_number}_DEG_data.txt"
@@ -238,7 +276,7 @@ def transcriptome(args):
         
         # R pathview 画图
         if args.draw_pathview:
-            print(f"正在画 {compare_info} 的 pathview 图")
+            logger.info(f"正在画 {compare_info} 的 pathview 图")
             draw_pathview(f"{compare_info}_regulation.txt", f"{compare_info}_ko_passed_path.txt")
         
         # 对每个比较组的文件进行整理
@@ -257,15 +295,25 @@ def passed_path(ko_file, output_prefix):
 
 
 def get_ko_expression(ko, kegg_clean_file, expression_file):
+    """对每个 ko 相关的基因生成一个 ko 相关基因的表达量表
+
+    Args:
+        ko (_type_): ko_list 文件
+        kegg_clean_file (_type_): KEGG_clean.txt
+        expression_file (_type_): fpkm_matrix
+    """
     if not os.path.exists(ko):
-        sys.exit(f"Error: {ko} not found")
+        logger.critical(f"{ko} not found")
+        sys.exit(1)
     if not os.path.exists(kegg_clean_file):
-        sys.exit(f"Error: {kegg_clean_file} not found")
+        logger.critical(f'{kegg_clean_file} not found')
+        sys.exit(1)
     if not os.path.exists(expression_file):
-        sys.exit(f"Error: {expression_file} not found")
+        logger.critical(f"{expression_file} not found")
+        sys.exit(1)
         
-    ko_list = open(ko, 'r').read().strip().split('\n')
-    print(f"正在输出每个 ko_pathway 相关的基因表达量表：{len(ko_list)} 个 ko_pathway")
+    ko_list = pd.read_csv(args.ko_list, sep='\t', usecols=[0]).values.tolist()
+    logger.info(f"正在输出每个 ko_pathway 相关的基因表达量表：{len(ko_list)} 个 ko_pathway")
     for ko_num in ko_list:
         kegg_pathway_df = pd.read_csv(kegg_clean_file, sep='\t', names=['GeneID', 'Ko'], usecols=[0, 1], dtype=str)
         kegg_pathway_df['Ko_Number'] = kegg_pathway_df['Ko'].str.split(":").str[0]
@@ -274,9 +322,9 @@ def get_ko_expression(ko, kegg_clean_file, expression_file):
         expression_df = expression_df[expression_df['GeneID'].isin(kegg_pathway_df['GeneID'])]
         if expression_df.shape[0] > 0:
             expression_df.to_csv(ko_num + '_expression.txt', sep='\t', index=False)
-            print(f"正在输出 {ko_num} 相关的基因表达量表，有 {expression_df.shape[0]} 个基因")
+            logger.info(f"正在输出 {ko_num} 相关的基因表达量表，有 {expression_df.shape[0]} 个基因")
         else:
-            print(f"{ko_num} 相关的基因表达量表为空")
+            logger.warning(f"{ko_num} 相关的基因表达量表为空")
 
 
 def draw_pathview(regulation, passed_path):
@@ -285,9 +333,9 @@ def draw_pathview(regulation, passed_path):
         -p {passed_path}"
     ret = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if ret.returncode != 0:
-        print(f"{regulation} draw_pathview 画图失败")
-        print(f"标准输出：{ret.stdout.decode()}")
-        print(f"标准错误: {ret.stderr.decode()}")
+        logger.error(f"{regulation} draw_pathview 画图失败")
+        logger.error(f"标准输出：{ret.stdout.decode()}")
+        logger.error(f"标准错误: {ret.stderr.decode()}")
 
 
 def draw_multigroup_heatmap(datafile):
@@ -314,27 +362,27 @@ def draw_multigroup_heatmap(datafile):
     cmd = f"Rscript /home/colddata/qinqiang/script/Rscript/heatmap/heatmap_multigroup.r -f {datafile}"
     ret = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if ret.returncode != 0:
-        print(f"{datafile} multigroup_heatmap 画图失败")
-        print(f"标准输出：{ret.stdout.decode()}")
-        print(f"标准错误: {ret.stderr.decode()}")
+        logger.error(f"{datafile} multigroup_heatmap 画图失败")
+        logger.error(f"标准输出：{ret.stdout.decode()}")
+        logger.error(f"标准错误: {ret.stderr.decode()}")
 
 
 def draw_twogroup_heatmap(datafile, output_file):
     cmd = f"Rscript /home/colddata/qinqiang/script/Rscript/heatmap/heatmap_twogroup.r -f {datafile} -o {output_file}"
     ret = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if ret.returncode != 0:
-        print(f"{datafile} twogroup_heatmap 画图失败")
-        print(f"标准输出：{ret.stdout.decode()}")
-        print(f"标准错误: {ret.stderr.decode()}")
+        logger.error(f"{datafile} twogroup_heatmap 画图失败")
+        logger.error(f"标准输出：{ret.stdout.decode()}")
+        logger.error(f"标准错误: {ret.stderr.decode()}")
 
 
 def anova_analysis(datafile, samples_file, output_file):
     cmd = f"Rscript /home/colddata/qinqiang/script/Rscript/anova/anova.r -f {datafile} -s {samples_file} -o {output_file}"
     ret = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if ret.returncode != 0:
-        print(f"{datafile} twogroup_heatmap 画图失败")
-        print(f"标准输出：{ret.stdout.decode()}")
-        print(f"标准错误: {ret.stderr.decode()}")
+        logger.error(f"{datafile} twogroup_heatmap 画图失败")
+        logger.error(f"标准输出：{ret.stdout.decode()}")
+        logger.error(f"标准错误: {ret.stderr.decode()}")
 
 
 def parse_input():
@@ -357,20 +405,12 @@ def parse_input():
     # 针对转录组，de results 结果 up down 的 id list 画 pahtview 图
     group2 = argparser.add_argument_group('针对转录组的使用参数，如果跑转录组的项目，下面必须的参数必须输入')
     
-    # 2024_04_25 使用 --kegg-clean 一个文件就可以了，少写一些参数
-    # group2.add_argument('--kegg-clean', dest='kegg_clean', type=str, help="[必须]输入 kegg 注释出来的 'KEGG_clean 文件'")
     group2.add_argument('--deg-data-dir', dest='deg_data_dir', type=str, help='[必须]输入 DEG_data.txt 文件')
     group2.add_argument('-s', '--samplesinfo', type=str, help='[必须]输入样品信息文件，如果有则添加到输出文件中')
-    
-    # 添加了 deg-data 参数，下面这些无需使用
-    # group2.add_argument('-c', '--compareinfo', type=str, help='[必须]输入比较信息文件，如果有则添加到输出文件中')
-    # group2.add_argument('--fpkm', type=str, help='[可选]输入 fpkm 文件，如果有则添加到输出文件中')
-    
     
     # 画热图添加的参数    
     group2.add_argument('--heatmap', action='store_true',
                         help='[可选]整理表达量文件，输出差异基因的热图')
-    # # 2024_04_11 不用弄颜色了，自动分配了
     group2.add_argument('--treat-color', dest="treat_color", type=str, default="blue",
                         help='[可选]画 heatmap 时指定 Treat Group 的颜色，默认 blue')
     group2.add_argument('--control-color', dest="control_color", type=str, default="red",
@@ -389,9 +429,11 @@ def main():
     
     else:
         if not os.path.exists(args.kegg_genedef):
-            sys.exit(f"Error: {args.kegg_genedef} not found")
+            logger.critical(f"Error: {args.kegg_genedef} not found")
+            sys.exit(1)
         if not os.path.exists(args.kegg_clean):
-            sys.exit(f"Error: {args.kegg_clean} not found")
+            logger.critical(f"Error: {args.kegg_clean} not found")
+            sys.exit(1)
         
         other(args)
         
@@ -399,14 +441,14 @@ def main():
         passed_path(args.ko_list, args.output_prefix)
         # 画 pathview 图
         if args.draw_pathview:
-            print("正在画 pathview 图")
+            logger.info("正在画 pathview 图")
             draw_pathview(args.output_prefix + '_regulation.txt', args.output_prefix + '_ko_passed_path.txt')
             
         # 出表达量表
         if args.expression and args.kegg_clean:
             get_ko_expression(args.ko_list, args.kegg_clean, args.expression)
     
-    print('\nDone!\n')
+    logger.info('Done!')
     
 
 if __name__ == '__main__':
