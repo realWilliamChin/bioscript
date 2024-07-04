fd0b4ca11265ee943ea9807718677786d30220df
WilliamGoGo <realwilliamchin@outlook.com>
2024-07-04 11:03:05 +0800

    [PERF] Plot/Quadrant/9QuadrantDiagram.r 优化输入参数和输出参数

--- Plot/Quadrant/9QuadrantDiagram.r
+++ Plot/Quadrant/9QuadrantDiagram.r
@@ -1,7 +1,12 @@
-setwd("/home/colddata/qinqiang/Project/2024_06_07_renminyiyuan_lixuewei_ji/联合分析")
+# setwd("/home/colddata/qinqiang/Project/2024_06_07_renminyiyuan_lixuewei_ji/联合分析")
 library(ggplot2)
 library(ggthemes)
-data.xx<-read.table("result_group.txt",header = T,check.names = F)
+
+args=commandArgs(T)
+input_file <- args[1]
+output_file <- args[2]
+
+data.xx<-read.table(input_file,header = T,check.names = F)
 head(data.xx)
 colnames(data.xx)
 ggplot(data.xx,aes(x=log2FC_RNA,y=log2FC_protein,colour=Group))+geom_point()+
@@ -10,5 +15,5 @@ ggplot(data.xx,aes(x=log2FC_RNA,y=log2FC_protein,colour=Group))+geom_point()+
   geom_vline(xintercept = -0.26, color = "grey", linetype = "dashed")+
   geom_hline(yintercept = 0.26, color = "grey", linetype = "dashed") +
   geom_hline(yintercept = -0.26, color = "grey", linetype = "dashed") 
-
+ggsave(output_file, width = 6, height = 6, dpi = 300)
 #length(data.xx$Group[data.xx$Group=="group5"])
