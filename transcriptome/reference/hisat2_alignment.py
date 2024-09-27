7e29d056f9672995a856cbcf36ec73fbe9d8debf
WilliamGoGo <realwilliamchin@outlook.com>
2024-09-27 10:31:46 +0800

    [UPDATE] transcriptome/reference/hisat2_alignment.py 更新 bowtie2 比对

--- transcriptome/reference/hisat2_alignment.py
+++ transcriptome/reference/hisat2_alignment.py
@@ -11,11 +11,13 @@ from loguru import logger
 
 def parse_input():
     args = argparse.ArgumentParser(description='hisat2_mapping_summary.py')
+    args.add_argument(dest='alignment_type', choices=['hisat2', 'bowtie2'], help='使用哪种方式比对')
     args.add_argument('-s', '--samples', help='samples_described.txt', default='samples_described.txt')
-    args.add_argument('--md', help='input hisat2 mapping dir')
+    args.add_argument('--gff', help='stringtie 所需的 gff 文件')
+    args.add_argument('--md', help='hisat2 mapping dir')
     args.add_argument('--cd', help='cleandata file dir', default='./02_Cleandata')
     
-    args.add_argument('--bd', help='input bam output dir')
+    args.add_argument('--bd', help='bam output dir')
     args.add_argument('--ref', help='reference specie')
     args.add_argument('--cpu', help='运行线程数量', default=30)
     args.add_argument('--msf', help='output mapping summary file', default='mapping_summary.txt')
@@ -23,7 +25,7 @@ def parse_input():
     return args.parse_args()
 
 
-def check_cleandata(samples_file, cleandata_dir):
+def check_data(samples_file, cleandata_dir):
     """检查 cleandata 文件是否存在
 
     Args:
@@ -39,10 +41,14 @@ def check_cleandata(samples_file, cleandata_dir):
             logger.error(f'文件 {file1} 或 {file2} 不存在，将跳过此样本 {samples_name} 的比对')
             continue
     logger.info('cleandata 文件检查完毕')
+    
+    # if not os.access(f'{ref_data}.1.ht2', os.R_OK):
+    #    logger.error(f'文件 {ref_data} 不存在，将退出程序')
+    #    sys.exit(1)
 
 
-def hisat2_alignment(samples_file, cleandata_dir, mapping_file_dir, bam_file_dir, ref_specie, num_threads):
-    """# hisat2 alignment 比对
+def alignment(alignment_type, samples_file, cleandata_dir, mapping_file_dir, bam_file_dir, ref_specie, num_threads, gff_file=None):
+    """# hisat2 and bowtie2 alignment 比对
 
     Args:
         samples_file (str): samples_described.txt
@@ -51,6 +57,8 @@ def hisat2_alignment(samples_file, cleandata_dir, mapping_file_dir, bam_file_dir
         ref_specie (str): 参考物种的路径路径
         num_threads (str): 运行线程数量
     """
+    os.makedirs(mapping_file_dir, exist_ok=True)
+    os.makedirs(bam_file_dir, exist_ok=True)
     samples_df = pd.read_csv(samples_file, sep='\t', usecols=['sample', 'filename1', 'filename2'])
 
     for index, row in samples_df.iterrows():
@@ -62,28 +70,47 @@ def hisat2_alignment(samples_file, cleandata_dir, mapping_file_dir, bam_file_dir
         bam_file_name = os.path.join(bam_file_dir, f'{samples_name}.bam')
         
         logger.info(f'开始比对样本 {samples_name}, {file1} 和 {file2}，输出文件 {mapping_file_name} 和 {bam_file_name}')
-        cmd = f'hisat2 -x {ref_specie} \
-            -p {num_threads} \
-            -I 200 -X 400 --fr \
-            --min-intronlen 20 --max-intronlen 4000 \
-            -1 {file1} \
-            -2 {file2} \
-            2> {mapping_file_name} | \
-            samtools sort --threads {num_threads} -O BAM -o - > {bam_file_name}'
-        # logger.debug(cmd)
-        rep = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        if alignment_type == 'hisat2':
+            alignment_cmd = f'hisat2 -x {ref_specie} \
+                -p {num_threads} \
+                -I 200 -X 400 --fr \
+                --min-intronlen 20 --max-intronlen 4000 \
+                -1 {file1} \
+                -2 {file2} \
+                2> {mapping_file_name} | \
+                samtools sort --threads {num_threads} -O BAM -o - > {bam_file_name}'
+        elif alignment_type == 'bowtie2':
+            alignment_cmd = f'bowtie2 -x {ref_specie} \
+                -p {num_threads} \
+                -1 {file1} \
+                -2 {file2} \
+                2> {mapping_file_name} | \
+                samtools sort --threads {num_threads} -O BAM -o - > {bam_file_name}'
+        
+        rep = subprocess.run(alignment_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
         if rep.returncode != 0:
             logger.error(f'样本 {samples_name} 比对失败')
             logger.error(rep.stderr.decode())
             logger.error(rep.stdout.decode())
         else:
-            logger.info(f'样本 {samples_name} 比对成功')
             with open(mapping_file_name) as f:
                 last_line = f.readlines()[-1]
-                logger.info(last_line)
-    
+                logger.success(f'样本 {samples_name} 比对成功 {last_line}')
+        
+        # TODO: gff 未进行测试过
+        if gff_file:
+            stringtie_cmd = f'nohup stringtie -e -B \
+                -G {gff_file} \
+                -A {bam_file_dir}/fpkm/{samples_name}_fpkm.txt \
+                -o {bam_file_dir}/ballgown/{samples_name}/{samples_name}.gtf \
+                {bam_file_name} > /dev/null 2>&1 &'
+            logger.debug(stringtie_cmd)
+            os.system(stringtie_cmd)
 
-def process_mapping(mapping_file_dir, samples_file, output_file):
+
+def summary_hisat2_mapping(mapping_file_dir, samples_file, output_file):
     """ 对所有比对文件进行汇总，输出一个总的比对结果
 
     Args:
@@ -114,17 +141,13 @@ def process_mapping(mapping_file_dir, samples_file, output_file):
         open(output_file, 'a').write(f'{sample}\t{total_reads}\t{mapped_reads}\t{unmapped_reads}\t{unique_mapped}\t{multi_mapped_reads}\t{overall_rate}\n')
 
 
-def main():
+if __name__ == '__main__':
     args = parse_input()
     if args.cd:
-        check_cleandata(args.samples, args.cd)
-        hisat2_alignment(args.samples, args.cd, args.md, args.bd, args.ref, args.cpu)
+        check_data(args.samples, args.cd)
+        alignment(args.alignment_type, args.samples, args.cd, args.md, args.bd, args.ref, args.cpu, args.gff)
     mapping_summary_file = os.path.join(args.md, args.msf)
-    process_mapping(args.md, args.samples, mapping_summary_file)
-
-
-if __name__ == '__main__':
-    main()
+    summary_hisat2_mapping(args.md, args.samples, mapping_summary_file)
 
 
 
