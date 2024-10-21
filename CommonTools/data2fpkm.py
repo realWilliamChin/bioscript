234ee3263588303aa21caf3fd059344875ac3bbd
WilliamGoGo <realwilliamchin@outlook.com>
2024-10-21 11:37:29 +0800

    [PERF] 添加 loadinput

--- CommonTools/data2fpkm.py
+++ CommonTools/data2fpkm.py
@@ -3,9 +3,13 @@
 # Created Time  : 2024/09/29 13:58
 # Author        : William GoGo
 import os, sys
+import numpy as np
 import pandas as pd
 import argparse
 from loguru import logger
+
+sys.path.append(os.path.abspath('/home/colddata/qinqiang/script/CommonTools/'))
+from load_input import load_table, write_output_df
 
 
 def parse_input():
@@ -17,19 +21,21 @@ def parse_input():
 
 
 def data2fpkm(input_file, output_file):
-    df = pd.read_csv(input_file)
+    df = load_table(input_file)
     
     if not df.iloc[:, 1:].apply(pd.to_numeric, errors='coerce').notnull().all().all():
         logger.critical("输入文件中包含非数值数据，请检查。")
         sys.exit(1)
     
     row_names = df.iloc[:, 0]
+    
     column_sums = df.iloc[:, 1:].sum(axis=0)
+    column_sums = np.where(column_sums == 0, 1, column_sums)
     normalized_df = df.iloc[:, 1:].div(column_sums, axis=1) * 100
     normalized_df.insert(0, df.columns[0], row_names)
     
     # 输出到文件
-    normalized_df.to_csv(output_file, sep='\t', index=False)
+    write_output_df(normalized_df, output_file, index=False)
 
 
 if __name__ == "__main__":
