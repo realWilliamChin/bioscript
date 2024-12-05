b19bc6b9485cb81ef34e117a1626b798dd05bf15
WilliamGoGo <realwilliamchin@outlook.com>
2024-12-05 15:06:27 +0800

    [UPDATE] transcriptome/noreference/count_filtered.py transcriptome/noreference/fpkm_filtered.py 增加读取输出参数

--- transcriptome/noreference/count_filtered.py
+++ transcriptome/noreference/count_filtered.py
@@ -1,50 +1,52 @@
 # -*- coding: UTF-8 -*-
+import os, sys
 import argparse
-import os
-from shutil import copyfile
-import sys
-import numpy
-
-import sys
-
+from loguru import logger
 
 
 
 #os.system("sed 's/,/\t/g' gene_count_matrix.csv > gene_count_matrix.txt")
-parser = argparse.ArgumentParser(description=' ')
+def parse_input():
+    parser = argparse.ArgumentParser(description=' ')
 
-parser.add_argument('-n', type=int, default=50)
+    parser.add_argument('-n', type=int, default=50)
+    parser.add_argument('-i', default='gene_count_matrix.txt')
+    parser.add_argument('-o', default='reads_matrix_filtered.txt')
 
-args = parser.parse_args()
-greater_than = int(args.n)
-
-
-f2=open('reads_matrix_filtered.txt','w')
-num=0
-num1=0
-
-with open('gene_count_matrix.txt','r') as f1:
-    for line in f1.readlines():
-        line=line.strip()
-        if(num==0):
-            each_part=line.split('\t')
-            f2.write( 'GeneID'+ '\t')
-            #for each in each_part[1:]:
-            f2.write(str('\t'.join(each_part[1:])))
-            print(str('\t'.join(each_part[1:])))
-            f2.write('\n')
-        else:
-            each_part = line.split('\t')
-            for each in each_part[1:]:
-                if(float(each)>greater_than):
-                    line1=line.replace('\t','\t')
-                    f2.write(line1)
-                    f2.write('\n')
-                    num1+=1
-                    break
+    return parser.parse_args()
 
 
-        num+=1
-f2.close()
-print('total_filtered(not include head line):' + str(num1))
-print('total(include head line):'+str(num))
+if __name__ == '__main__':
+    args = parse_input()
+    
+    greater_than = int(args.n)
+    input_file = args.i
+    output_file = args.o
+
+    f2=open(output_file,'w')
+    num=0
+    num1=0
+
+    with open(input_file,'r') as f1:
+        for line in f1.readlines():
+            line=line.strip()
+            if(num==0):
+                each_part=line.split('\t')
+                f2.write( 'GeneID'+ '\t')
+                #for each in each_part[1:]:
+                f2.write(str('\t'.join(each_part[1:])))
+                print(str('\t'.join(each_part[1:])))
+                f2.write('\n')
+            else:
+                each_part = line.split('\t')
+                for each in each_part[1:]:
+                    if(float(each)>greater_than):
+                        line1=line.replace('\t','\t')
+                        f2.write(line1)
+                        f2.write('\n')
+                        num1+=1
+                        break
+            num+=1
+    f2.close()
+    logger.info('total_filtered(not include head line):' + str(num1))
+    logger.info('total(include head line):'+str(num))
--- transcriptome/noreference/fpkm_filtered.py
+++ transcriptome/noreference/fpkm_filtered.py
@@ -1,47 +1,59 @@
 # -*- coding: UTF-8 -*-
-
+import os, sys
 import argparse
-import os
-from shutil import copyfile
-import sys
-import numpy
+from loguru import logger
 
-import sys
-#os.system("sed 's/,/\t/g' gene_fpkm_matrix.csv > gene_fpkm_matrix.txt")
-num=0
-num1=0
-num3=0
-id_list=[]
-f3=open('reads_matrix_filtered.txt','r')
-for line in f3.readlines():
-        id=line.split('\t')[0]
-        id_list.extend([id])
-        num1+=1
-print('num of count_matri_filter(include head line)：'+str(num1))
-f2=open('fpkm_matrix_filtered.txt','w')
-num=0
-with open('gene_fpkm_matrix.txt','r') as f1:
-    for line in f1.readlines():
-        if (num == 0):
-            each_part = line.split('\t')
-            f2.write('GeneID' + '\t')
+def parse_input():
+    parser = argparse.ArgumentParser(description=' ')
 
-            #for each in each_part[1:]:
-            f2.write('\t'.join(each_part[1:]))
+    parser.add_argument('-r', default='reads_matrix_filtered.txt')
+    parser.add_argument('-i', default='gene_fpkm_matrix.txt')
+    parser.add_argument('-o', default='fpkm_matrix_filtered.txt')
+
+    return parser.parse_args()
 
 
-        else:
-            line=line.strip()
-            id_part=line.split('|')[0]
-            if ('|' not in line):
-                id_part=line.split('\t',1)[0]
+if __name__ == '__main__':
+    args = parse_input()
+    reads_file = args.r
+    input_file = args.i
+    output_file = args.o
+    
+    
+    num=0
+    num1=0
+    num3=0
+    id_list=[]
+    f3=open(reads_file,'r')
+    for line in f3.readlines():
+            id=line.split('\t')[0]
+            id_list.extend([id])
+            num1+=1
+    logger.info('num of count_matri_filter(include head line)：'+str(num1))
+    f2=open(output_file,'w')
+    num=0
+    with open(input_file,'r') as f1:
+        for line in f1.readlines():
+            if (num == 0):
+                each_part = line.split('\t')
+                f2.write('GeneID' + '\t')
 
-            if(id_part in id_list):
-                new_line=line.split('\t',1)[-1]
-                f2.write(id_part+'\t'+new_line+'\n')
-                num3+=1
-        num+=1
-f2.close()
-f3.close()
-print('mapped fpkm list(not include head line):'+str(num3))
-print('num of fpkm_matrix(include head line)'+str(num))
+                #for each in each_part[1:]:
+                f2.write('\t'.join(each_part[1:]))
+
+
+            else:
+                line=line.strip()
+                id_part=line.split('|')[0]
+                if ('|' not in line):
+                    id_part=line.split('\t',1)[0]
+
+                if(id_part in id_list):
+                    new_line=line.split('\t',1)[-1]
+                    f2.write(id_part+'\t'+new_line+'\n')
+                    num3+=1
+            num+=1
+    f2.close()
+    f3.close()
+    logger.info('mapped fpkm list(not include head line):'+str(num3))
+    logger.info('num of fpkm_matrix(include head line)'+str(num))
