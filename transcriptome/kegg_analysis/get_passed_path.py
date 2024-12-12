1cfcec2d70ec71921175e5eb26701afc11a183a6
WilliamGoGo <realwilliamchin@outlook.com>
2024-12-12 11:04:38 +0800

    [ALTER] transcriptome/kegg_analysis/get_passed_path.py 修改列名 KEGG_ID --> KEGG_Pathway_ID

--- transcriptome/kegg_analysis/get_passed_path.py
+++ transcriptome/kegg_analysis/get_passed_path.py
@@ -10,8 +10,8 @@ import argparse
 
 def parse_input():
     p = argparse.ArgumentParser()
-    p.add_argument(dest='ko_file', help='输入 KEGG_ID 文件')
-    p.add_argument(dest='output_file', help='输出文件')
+    p.add_argument(dest='ko_file', help='输入包含 KEGG_Pathway_ID 列文件')
+    p.add_argument(dest='output_file', help='输出文件 passed_path.txt')
     
     args = p.parse_args()
     
@@ -19,11 +19,11 @@ def parse_input():
 
 
 def passed_path(ko_file, output_file):
-    ko_def_df = pd.read_csv(ko_file, sep='\t', names=['KEGG_ID'], dtype=str, usecols=[0], header=0)
+    ko_def_df = pd.read_csv(ko_file, sep='\t', names=['KEGG_Pathway_ID'], dtype=str, usecols=[0], header=0)
     passed_path_file = '/home/colddata/qinqiang/script/Rscript/pathview/passed_path.txt'
-    passed_path_df = pd.read_csv(passed_path_file, sep='\t', names=['KEGG_ID', 'Ko_Def'], dtype=str)
-    ko_def_df = passed_path_df[passed_path_df['KEGG_ID'].isin(ko_def_df['KEGG_ID'])]
-    ko_def_df = ko_def_df.drop_duplicates(subset=['KEGG_ID'])
+    passed_path_df = pd.read_csv(passed_path_file, sep='\t', names=['KEGG_Pathway_ID', 'Ko_Def'], dtype=str)
+    ko_def_df = passed_path_df[passed_path_df['KEGG_Pathway_ID'].isin(ko_def_df['KEGG_Pathway_ID'])]
+    ko_def_df = ko_def_df.drop_duplicates(subset=['KEGG_Pathway_ID'])
     ko_def_df.to_csv(output_file, sep='\t', index=False, header=False)
 
 
