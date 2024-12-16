0a2fecebb3b2cb273ef069fd47e3a31b2e1d8e55
WilliamGoGo <realwilliamchin@outlook.com>
2024-12-16 12:35:18 +0800

    [NEW FEATURE] cleandata_alignment.py 添加 bwa 和 salmon 比对

--- cleandata_alignment.py
+++ cleandata_alignment.py
@@ -8,46 +8,40 @@ import argparse
 import subprocess
 from loguru import logger
 
+sys.path.append(os.path.abspath('/home/colddata/qinqiang/script/CommonTools/'))
+from check_cleandata_samples import check_data
+
 
 def parse_input():
-    args = argparse.ArgumentParser(description='hisat2_mapping_summary.py')
-    args.add_argument(dest='alignment_type', choices=['hisat2', 'bowtie2', 'bwa'], help='使用哪种方式比对')
-    args.add_argument('-s', '--samples', help='samples_described.txt', default='samples_described.txt')
+    args = argparse.ArgumentParser(
+        description='hisat2_mapping_summary.py',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    # 基础参数
+    args.add_argument(
+        dest='alignment_type', 
+        choices=['hisat2', 'bowtie2', 'bwa', 'salmon'], 
+        help=(
+            "选择使用哪种方式比对，并确保提供必要的参数"
+        )
+    )
+    args.add_argument('-s', '--samples', required=True, help='samples_described.txt (默认: samples_described.txt)', default='samples_described.txt')
     args.add_argument('--gff', help='[有参流程] stringtie 所需的 gff 文件')
-    args.add_argument('--md', help='hisat2 mapping dir')
-    args.add_argument('--cd', help='cleandata file dir', default='./02_Cleandata')
-    
-    args.add_argument('--bd', help='bam output dir')
-    args.add_argument('--ref', help='reference specie')
-    args.add_argument('--cpu', help='运行线程数量', default=30)
-    args.add_argument('--msf', help='output mapping summary file', default='mapping_summary.txt')
-    
-    return args.parse_args()
+    args.add_argument('--md', help='比对总结结果文件夹')
+    args.add_argument('--cd', help='cleandata file dir')
+    args.add_argument('--result-dir', dest='result_dir', help='输出结果文件夹')
+    args.add_argument('--ref', help='reference index，运行 salmon 时只需要输入目录')
+    args.add_argument('--cpu', help='运行线程数量 (默认: 30)', default=30, type=int)
+    args.add_argument('--msf', help='output mapping summary file (默认: mapping_summary.txt)', default='mapping_summary.txt')
+
+    parsed_args = args.parse_args()
 
 
-def check_data(samples_file, cleandata_dir):
-    """检查 cleandata 文件是否存在
-
-    Args:
-        samples_file (str): samples_described.txt
-        cleandata_dir (str): cleandata 文件路径
-    """
-    samples_df = pd.read_csv(samples_file, sep='\t', usecols=['sample', 'filename1', 'filename2'])
-    for index, row in samples_df.iterrows():
-        samples_name = row['sample']
-        file1 = os.path.join(cleandata_dir, row['filename1'])
-        file2 = os.path.join(cleandata_dir, row['filename2'])
-        if not os.access(file1, os.R_OK) or not os.access(file2, os.R_OK):
-            logger.error(f'文件 {file1} 或 {file2} 不存在，将跳过此样本 {samples_name} 的比对')
-            continue
-    logger.info('cleandata 文件检查完毕')
-    
-    # if not os.access(f'{ref_data}.1.ht2', os.R_OK):
-    #    logger.error(f'文件 {ref_data} 不存在，将退出程序')
-    #    sys.exit(1)
+    return parsed_args
 
 
-def alignment(alignment_type, samples_file, cleandata_dir, mapping_file_dir, bam_file_dir, ref_specie, num_threads, gff_file=None):
+def alignment(alignment_type, samples_file, cleandata_dir, mapping_file_dir, bam_file_dir, ref_index, num_threads, gff_file=None):
     """# hisat2 and bowtie2 alignment 比对
 
     Args:
@@ -69,10 +63,10 @@ def alignment(alignment_type, samples_file, cleandata_dir, mapping_file_dir, bam
         mapping_file_name = os.path.join(mapping_file_dir, f'{sample_name}_mapping.txt')
         bam_file_name = os.path.join(bam_file_dir, f'{sample_name}.bam')
         
-        logger.info(f'开始比对样本 {sample_name}, {file1} 和 {file2}，输出文件 {mapping_file_name} 和 {bam_file_name}')
+        logger.info(f'开始比对样本 {sample_name}, {file1} 和 {file2}')
         
-        if alignment_type == 'hisat2':
-            alignment_cmd = f'hisat2 -x {ref_specie} \
+        if alignment_type.lower() == 'hisat2':
+            alignment_cmd = f'hisat2 -x {ref_index} \
                 -p {num_threads} \
                 -I 200 -X 400 --fr \
                 --min-intronlen 20 --max-intronlen 4000 \
@@ -80,20 +74,31 @@ def alignment(alignment_type, samples_file, cleandata_dir, mapping_file_dir, bam
                 -2 {file2} \
                 2> {mapping_file_name} | \
                 samtools sort --threads {num_threads} -O BAM -o - > {bam_file_name}'
-        elif alignment_type == 'bowtie2':
-            alignment_cmd = f'bowtie2 -x {ref_specie} \
+        elif alignment_type.lower() == 'bowtie2':
+            alignment_cmd = f'bowtie2 -x {ref_index} \
                 -p {num_threads} \
                 -1 {file1} \
                 -2 {file2} \
                 2> {mapping_file_name} | \
                 samtools sort --threads {num_threads} -O BAM -o - > {bam_file_name}'
-        elif alignment_type == 'bwa':
+        elif alignment_type.lower() == 'bwa':
             alignment_cmd = f'bwa mem -M \
                 -t {num_threads} \
                 -R "@RG\\tID:{sample_name}\\tSM:{sample_name}\\tLB:WES\\tPL:Illumina" \
-                {ref_specie} \
+                {ref_index} \
                 {file1} {file2} |\
                 samtools sort -O bam -@ {num_threads} -o - > {bam_file_name}'
+        elif alignment_type.lower() == 'salmon':
+            gene_map_file = os.path.join(ref_index, 'gene_map.txt')
+            salmon_output_dir = os.path.join(bam_file_dir, f'{sample_name}_gene_quant')
+            alignment_cmd = f'/opt/biosoft/salmon-latest_linux_x86_64/bin/salmon quant \
+                -p {num_threads} \
+                --validateMappings \
+                -i {ref_index} \
+                -l A \
+                -g {gene_map_file} \
+                -1 {file1} -2 {file2} \
+                -o {salmon_output_dir}'
         
         rep = subprocess.run(alignment_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
@@ -101,6 +106,8 @@ def alignment(alignment_type, samples_file, cleandata_dir, mapping_file_dir, bam
             logger.error(f'样本 {sample_name} 比对失败')
             logger.error(rep.stderr.decode())
             logger.error(rep.stdout.decode())
+        elif alignment_type.lower() == 'salmon':
+            continue
         else:
             with open(mapping_file_name) as f:
                 last_line = f.readlines()[-1]
@@ -151,7 +158,7 @@ if __name__ == '__main__':
     args = parse_input()
     if args.cd:
         check_data(args.samples, args.cd)
-        alignment(args.alignment_type, args.samples, args.cd, args.md, args.bd, args.ref, args.cpu, args.gff)
+        alignment(args.alignment_type, args.samples, args.cd, args.md, args.result_dir, args.ref, args.cpu, args.gff)
     mapping_summary_file = os.path.join(args.md, args.msf)
     summary_hisat2_mapping(args.md, args.samples, mapping_summary_file)
 
