dc50188df46039abc6fa0a817b5b1cd6558d274a
WilliamGoGo <realwilliamchin@outlook.com>
2024-12-19 16:44:28 +0800

    [NEW FILE] CommonTools/Fasta/newid.py 按照数字顺序对 fasta 基因重命名

---
+++ CommonTools/Fasta/newid.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created Time  : 2023/06/14 17:37
+# Author        : William GoGo
+import argparse
+from Bio import SeqIO
+from collections import defaultdict
+
+def rename_fasta(input_file, output_file, prefix='G_', start_num=1):
+    """
+    重命名 FASTA 文件中的基因名称，优化版。
+
+    Args:
+        input_file (str): 输入 FASTA 文件路径。
+        output_file (str): 输出 FASTA 文件路径。
+        start_num (int, optional): 新基因名称的起始编号。默认值为 1。
+    """
+
+    id_to_index = defaultdict(list)
+    for i, record in enumerate(SeqIO.parse(input_file, "fasta")):
+        id_to_index[record.id].append(i)
+
+    # 处理基因 ID 重复
+    for gene_id, indices in id_to_index.items():
+        if len(indices) > 1:
+            print(f"Warning: Gene ID {gene_id} is duplicated.")
+
+    with open(output_file, 'w') as output_handle:
+        for i, record in enumerate(SeqIO.parse(input_file, "fasta")):
+            gene_num = start_num + i
+            num_zeros = max(0, 7 - len(str(gene_num)))
+            new_id = f"{prefix}{'0'*num_zeros}{gene_num}"
+            record.id = new_id
+            record.description = ""
+            SeqIO.write(record, output_handle, "fasta")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Rename genes in a FASTA file')
+    parser.add_argument('-i', '--input', required=True, help='Input FASTA file')
+    parser.add_argument('-o', '--output', required=True, help='Output FASTA file')
+    parser.add_argument('-p', '--prefix', default='G_', help='Gene 名称前缀，默认是 G_')
+    parser.add_argument('-s', '--start', type=int, default=1, help='Starting number for new gene names')
+    args = parser.parse_args()
+
+    rename_fasta(args.input, args.output, args.start)
