bc785901a1bb29f51ff8ef1c96128324b580da42
WilliamGoGo <realwilliamchin@outlook.com>
2025-04-01 17:45:50 +0800

    [REFACTOR] Analysis/Timelapse/timelapse_cluster_enrich.py --> Analysis/enrich_analysis/geneids_enrich.py 重写通用版本的

--- Analysis/enrich_analysis/geneids_enrich.py
+++ Analysis/enrich_analysis/geneids_enrich.py
@@ -5,29 +5,54 @@
 import os, sys
 import argparse
 import pandas as pd
+from loguru import logger
 
 sys.path.append(os.path.abspath('/home/colddata/qinqiang/script/Rscript/'))
 from Rscript import enrich_analysis
 
-def parse_input():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--clusterid', help='提供 compareinfo up 和 down gene id 的目录，通常是./DEG_analysis_results')
-    parser.add_argument('-k', '--keggclean', help='kegg_clean.txt 文件')
-    parser.add_argument('-g', '--genego', help='kegg_clean.txt 文件')
-    parser.add_argument('-o', '--outputdir', help='文件输出目录')
+sys.path.append(os.path.abspath('/home/colddata/qinqiang/script/CommonTools/'))
+from load_input import load_table, write_output_df
 
-    return parser.parse_args()
+def parse_input():
+    p = argparse.ArgumentParser()
+    p.add_argument('-i', '--ids-dir', dest='ids_dir', help='cluster id 文件夹')
+    p.add_argument('--ids-header', dest='ids_header', action='store_true',
+                        help='所有 id 文件是否有 header(GeneID)')
+    p.add_argument('-k', '--kegg-clean', dest="kegg_clean", required=True,
+                   help='kegg_clean.txt 文件')
+    p.add_argument('-g', '--gene-go', dest="gene_go", required=True,
+                   help='swiss 注释出来的的 gene_go.txt 文件')
+    p.add_argument('-o', '--output-dir', dest='output_dir', help='文件输出目录')
+    
+    args = p.parse_args()
+    
+    if args.ids_header:
+        args.ids_header = 0
+    else:
+        args.ids_header = None
+
+    return args
 
 
 def main():
     args = parse_input()
-    cluster_id_file = args.clusterid
-    cluster_id_df = pd.read_csv(cluster_id_file, sep='\t', usecols=[0])
-    
-    tmp_file = cluster_id_file.replace('.txt', '_ID.txt')
-    cluster_id_df.to_csv(tmp_file, sep='\t', index=False, header=False)
-    enrich_analysis(tmp_file, args.genego, args.keggclean, args.outputdir)
+    ids_dir, ids_header = args.ids_dir, args.ids_header
+    for module_file in os.listdir(ids_dir):
+        logger.info(f'正在处理 {module_file}')
+        module_df = load_table(
+            os.path.join(ids_dir, module_file),
+            header=ids_header,
+            usecols=[0],
+            names=['GeneID'],
+            dtype={"GeneID": str}
+        )
+        
+        module_name = module_file.split('_')[0]
+        tmp_file = os.path.join(args.output_dir, f'{module_name}_ID.txt')
+        write_output_df(module_df, tmp_file, index=False, header=False)
+        enrich_analysis(tmp_file, args.gene_go, args.kegg_clean, args.output_dir)
 
+    logger.success('Done!')
 
 if __name__ == '__main__':
     main()
