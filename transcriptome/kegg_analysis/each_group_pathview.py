a43e296c850d0e5e83a8f676c32f5658da0e16b4
WilliamGoGo <realwilliamchin@outlook.com>
2025-04-07 18:02:22 +0800

    [REFACTOR] transcriptome/kegg_analysis/each_group_pathview.py 每个样本或者每个组的 pathview 画图

--- transcriptome/kegg_analysis/each_group_pathview.py
+++ transcriptome/kegg_analysis/each_group_pathview.py
@@ -12,7 +12,8 @@ from get_passed_path import passed_path
 def parse_input():
     p = argparse.ArgumentParser(description="")
     p.add_argument('--kid', help='输入的 KEGG_ID 文件')
-    p.add_argument('-f', '--files', nargs='+', help='每个样本的 reads count 文件', required=True)
+    p.add_argument('-f', '--files', nargs='+', required=True, 
+                   help='每个样本的 reads count 文件，或者包含所有样本的 reads_matrix_filtered.txt')
     p.add_argument('--keggclean', help='KEGG_clean 文件')
     
     args = p.parse_args()
@@ -20,7 +21,7 @@ def parse_input():
     return args
 
 
-if __name__ == '__main__':
+def main():
     args = parse_input()
     kegg_df = pd.read_csv(args.keggclean, sep='\t', header=None, usecols=[0, 1, 4], names=['GeneID', 'KO', 'KEGG_ID'])
     kegg_df['KO'] = kegg_df['KO'].str.split(':').str[0]
@@ -28,33 +29,64 @@ if __name__ == '__main__':
     path_df = pd.read_csv(path_file, sep='\t', header=None, names=['KO', 'Def'])
     # all_sample_df = pd.read_csv("gene_count_matrix.txt",sep='\t')
     # all_sample_list = all_sample_df.columns[1:]
-    all_sample_list = args.files
+    all_sample_file_list = args.files
+    
+    if len(all_sample_file_list) == 1:
+        all_sample_df = pd.read_csv(all_sample_file_list[0], sep='\t')
+        all_sample_list = all_sample_df.columns[1:]
+        for sample in all_sample_list:
+            os.mkdir(sample)
+            sample_df = all_sample_df[['GeneID', sample]].copy()
+            sample_df[sample].astype(float)
+            source_rows_number = sample_df.shape[0]
+            sample_df = sample_df[sample_df[sample] > 50]
+            sample_df = sample_df['GeneID']
+            filter_rows_number = sample_df.shape[0]
+            logger.info(f'{sample} 原始行数 {source_rows_number}，过滤完 50 之后的行数 {filter_rows_number}')
 
-    for sample_file in all_sample_list:
-        
-        # 这块儿筛选出每个组的 reads_count 
-        # sample_df = all_sample_df[['GeneID', sample]].copy()
-        # sample_df[sample].astype(float)
-        # source_rows_number = sample_df.shape[0]
-        # sample_df = sample_df[sample_df[sample] > 50]
-        # sample_df = sample_df['GeneID']
-        # filter_rows_number = sample_df.shape[0]
-        # print(f'{sample} 原始行数 {source_rows_number}，过滤完 {args.min} 之后的行数 {filter_rows_number}')
-        
-        sample_name = sample_file.replace('.txt', '')
-        os.mkdir(sample_name)
-        sample_df = pd.read_csv(sample_file, sep='\t')
+            result_df = pd.merge(sample_df, kegg_df, how='left', on='GeneID')
+            result_df['regulation'] =  1
+            result_df = result_df.drop_duplicates(subset=['KEGG_ID'])
+            result_df = result_df.drop(columns=['GeneID'])
+            result_df = result_df.dropna(subset='KEGG_ID')
+            if args.kid:
+                passed_path(args.kid, os.path.basename(args.kid).replace('.txt', '_passed_path.txt'))
+            else:
+                passed_path_df = pd.merge(result_df['KO'], path_df, on='KO', how='left')
+                passed_path_df.dropna(subset=['Def']).to_csv(f'{sample}/passed_path.txt', sep='\t', index=False, header=False)
+            
+            result_df.drop(columns=['KO']).to_csv(f'{sample}/regulation.txt', sep='\t', index=False)
+    
+    elif len(all_sample_file_list) > 1:
+        for sample_file in all_sample_file_list:
+            
+            # 这块儿筛选出每个组的 reads_count 
+            # sample_df = all_sample_df[['GeneID', sample]].copy()
+            # sample_df[sample].astype(float)
+            # source_rows_number = sample_df.shape[0]
+            # sample_df = sample_df[sample_df[sample] > 50]
+            # sample_df = sample_df['GeneID']
+            # filter_rows_number = sample_df.shape[0]
+            # print(f'{sample} 原始行数 {source_rows_number}，过滤完 {args.min} 之后的行数 {filter_rows_number}')
+            
+            sample_name = sample_file.replace('.txt', '')
+            os.mkdir(sample_name)
+            sample_df = pd.read_csv(sample_file, sep='\t')
 
-        result_df = pd.merge(sample_df, kegg_df, how='left', on='GeneID')
-        result_df['regulation'] =  1
-        result_df = result_df.drop_duplicates(subset=['KEGG_ID'])
-        result_df = result_df.drop(columns=['GeneID'])
-        result_df = result_df.dropna(subset=['KEGG_ID'])
-        if args.kid:
-            passed_path(args.kid, args.kid.replace('.txt', '_passed_path.txt'))
-        else:
-            passed_path_df = pd.merge(result_df['KO'], path_df, on='KO', how='left')
-            passed_path_df.dropna(subset=['Def']).to_csv(f'{sample_name}/passed_path.txt', sep='\t', index=False, header=False)
-        result_df = result_df.drop(columns=['KO'])
-        
-        result_df[['KEGG_ID', 'regulation']].to_csv(f'{sample_name}/regulation.txt', sep='\t', index=False)
+            result_df = pd.merge(sample_df, kegg_df, how='left', on='GeneID')
+            result_df['regulation'] =  1
+            result_df = result_df.drop_duplicates(subset=['KEGG_ID'])
+            result_df = result_df.drop(columns=['GeneID'])
+            result_df = result_df.dropna(subset=['KEGG_ID'])
+            if args.kid:
+                passed_path(args.kid, args.kid.replace('.txt', '_passed_path.txt'))
+            else:
+                passed_path_df = pd.merge(result_df['KO'], path_df, on='KO', how='left')
+                passed_path_df.dropna(subset=['Def']).to_csv(f'{sample_name}/passed_path.txt', sep='\t', index=False, header=False)
+            result_df = result_df.drop(columns=['KO'])
+            
+            result_df[['KEGG_ID', 'regulation']].to_csv(f'{sample_name}/regulation.txt', sep='\t', index=False)
+
+
+if __name__ == '__main__':
+    main()
