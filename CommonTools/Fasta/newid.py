833e927f74dec217d570560a6cd4ddae36ebde8b
WilliamGoGo <realwilliamchin@outlook.com>
2025-06-03 09:58:57 +0800

    [NEW FEATURE] CommonTools/Fasta/newid.py 识别基因数量，自动添加前缀0的数量

--- CommonTools/Fasta/newid.py
+++ CommonTools/Fasta/newid.py
@@ -6,6 +6,16 @@ import argparse
 from Bio import SeqIO
 from collections import defaultdict
 
+
+def get_fasta_gene_number(input_file):
+    gene_count = 0
+    with open(input_file, 'r') as f:
+        for line in f:
+            if line.startswith('>'):
+                gene_count += 1
+    return gene_count
+
+
 def rename_fasta(input_file, output_file, prefix='G_', start_num=1):
     """
     重命名 FASTA 文件中的基因名称，优化版。
@@ -13,6 +23,7 @@ def rename_fasta(input_file, output_file, prefix='G_', start_num=1):
     Args:
         input_file (str): 输入 FASTA 文件路径。
         output_file (str): 输出 FASTA 文件路径。
+        prefix (str): 输入前缀
         start_num (int, optional): 新基因名称的起始编号。默认值为 1。
     """
 
@@ -25,10 +36,12 @@ def rename_fasta(input_file, output_file, prefix='G_', start_num=1):
         if len(indices) > 1:
             print(f"Warning: Gene ID {gene_id} is duplicated.")
 
+    all_gene_count = get_fasta_gene_number(input_file)
+    num_digits = len(str(all_gene_count))
     with open(output_file, 'w') as output_handle:
         for i, record in enumerate(SeqIO.parse(input_file, "fasta")):
             gene_num = start_num + i
-            num_zeros = max(0, 7 - len(str(gene_num)))
+            num_zeros = max(0, num_digits - len(str(gene_num)))
             new_id = f"{prefix}{'0'*num_zeros}{gene_num}"
             record.id = new_id
             record.description = ""
@@ -42,4 +55,4 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--start', type=int, default=1, help='Starting number for new gene names')
     args = parser.parse_args()
 
-    rename_fasta(args.input, args.output, args.start)
+    rename_fasta(args.input, args.output, args.prefix, args.start)
