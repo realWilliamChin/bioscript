39a01167f25a0f10292b5fccf4ab328da9df9f08
WilliamGoGo <realwilliamchin@outlook.com>
2025-06-04 13:55:06 +0800

    [NEW FEATURE] CommonTools/Fasta/drop_duplicate.py 增加去重选项，保留第一个或者最长的序列

--- CommonTools/Fasta/drop_duplicate.py
+++ CommonTools/Fasta/drop_duplicate.py
@@ -11,31 +11,30 @@ def parse_arguments():
     parser.add_argument('-i', '--input', required=True, help='Input FASTA file')
     parser.add_argument('-o', '--output', required=True, help='Output FASTA file')
     parser.add_argument('-s', '--split', default='@@@', help='序列名以哪个字符串分割，默认不分割')
+    parser.add_argument('--keep', choices=['long', 'first'], default='long',
+                        help='去除重复的选项，选择保留最长的，或者是第一个')
     return parser.parse_args()
 
 
-def filter_duplicates(fasta_file, strsplit):
+def filter_duplicates(fasta_file, strsplit, keep='long'):
     sequences = {}
 
     # 读取 FASTA 文件，并将序列保存到字典中
     for record in SeqIO.parse(fasta_file, "fasta"):
-        # sequence_id = record.id
         sequence_id = record.id.split(strsplit, 1)[0]
         sequence = str(record.seq)
         length = len(sequence)
         
-        
-        # 如果序列已存在且长度更长，则更新字典中的值
-        if sequence_id in sequences and length > len(sequences[sequence_id][0]):
+        if sequence_id not in sequences:
             sequences[sequence_id] = (sequence, length)
-        # 如果序列不存在，则将其添加到字典中
-        elif sequence_id not in sequences:
+        elif keep == 'long' and length > len(sequences[sequence_id][0]):
             sequences[sequence_id] = (sequence, length)
+        # 如果 keep == 'first'，则保持第一个序列不变
 
-    # 返回保留最长序列的列表，包括原始的序列名和序列
-    longest_sequences = [(sequence_id, sequence) for sequence_id, (sequence, _) in sequences.items()]
+    # 返回保留的序列列表，包括原始的序列名和序列
+    filtered_sequences = [(sequence_id, sequence) for sequence_id, (sequence, _) in sequences.items()]
 
-    return longest_sequences
+    return filtered_sequences
 
 
 def write_sequences(output_file, sequences):
@@ -45,8 +44,10 @@ def write_sequences(output_file, sequences):
 
 def main():
     args = parse_arguments()
-    longest_sequences = filter_duplicates(args.input, args.split)
-    write_sequences(args.output, longest_sequences)
+    filtered_sequences = filter_duplicates(args.input, args.split, args.keep)
+    write_sequences(args.output, filtered_sequences)
+    logger.success(f"已成功去除重复序列，并保存到 {args.output} 文件中")
+
 
 if __name__ == '__main__':
     main()
