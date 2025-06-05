20fc0b43d60526575c1843f9b9908bdec83cf3ef
WilliamGoGo <realwilliamchin@outlook.com>
2025-06-05 17:01:41 +0800

    [PERF] transcriptome/transcriptome_enrich.py 优化输入输出，优化文件夹创建

--- transcriptome/transcriptome_enrich.py
+++ transcriptome/transcriptome_enrich.py
@@ -7,25 +7,32 @@ import argparse
 import pandas as pd
 from loguru import logger
 
-sys.path.append(os.path.abspath('/home/colddata/qinqiang/script/Rscript/'))
+sys.path.append('/home/colddata/qinqiang/script/Rscript/')
 from Rscript import enrich_analysis
+sys.path.append('/home/colddata/qinqiang/script/CommonTools')
+from load_input import load_table, write_output_df
+
 
 def parse_input():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--compare', default='compare_info.txt', type=str,
+    parser.add_argument('-c', '--compare', default='compare_info.txt',
                         help='compare_info.txt 文件，主要用来读取组间名称')
-    parser.add_argument('--degdata-dir', dest='degdata_dir',
+    parser.add_argument('--degdata-dir', dest='degdata_dir', default='DEG_analysis_results',
                         help='提供 compareinfo up 和 down gene id 的目录，通常是./DEG_analysis_results')
     parser.add_argument('-k', '--keggclean', help='kegg_clean.txt 文件')
     parser.add_argument('-g', '--genego', help='kegg_clean.txt 文件')
-    parser.add_argument('-o', '--outputdir', default=os.curdir())
+    parser.add_argument('-o', '--outputdir', default='Pathway_enrichment_analysis')
     
-    return parser.parse_args()
+    args = parser.parse_args()
+    
+    os.makedirs(args.outputdir, exist_ok=True)
+    
+    return args
 
 
-def transcriptome_enrich(compare, degdata_dir, genego_file, keggclean_file, outputdir):
-    compare_df = pd.read_csv(compare, sep='\t')
-    compare_df['compare_name'] = compare_df['Treat'] + '_vs_' + compare_df['Control']
+def deg_enrich(compare, degdata_dir, genego_file, keggclean_file, outputdir):
+    compare_df = load_table(compare)
+    compare_df['compare_name'] = compare_df['Treat'] + '-vs-' + compare_df['Control']
     for comp in compare_df['compare_name'].tolist():
         deg_Upid_file = os.path.join(degdata_dir, f'{comp}_Up_ID.txt')
         deg_Downid_file = os.path.join(degdata_dir, f'{comp}_Down_ID.txt')
@@ -40,7 +47,7 @@ def transcriptome_enrich(compare, degdata_dir, genego_file, keggclean_file, outp
 
 def main():
     args = parse_input()
-    transcriptome_enrich(args.compare, args.degdata_dir, args.genego, args.keggclean, args.outputdir)
+    deg_enrich(args.compare, args.degdata_dir, args.genego, args.keggclean, args.outputdir)
 
 
 if __name__ == '__main__':
