680c4c07b4e8a2255a1bde3797b4705a38666b5c
WilliamGoGo <realwilliamchin@outlook.com>
2025-07-07 16:37:12 +0800

    [PERF] transcriptome/genedf_add_expression_and_def.py 新增指定列合并，针对 GeneSymbol，其他代码优化

--- transcriptome/genedf_add_expression_and_def.py
+++ transcriptome/genedf_add_expression_and_def.py
@@ -14,15 +14,16 @@ from load_input import load_table, write_output_df
 
 def parse_input():
     parser = argparse.ArgumentParser(description='对各种表添加基因定义文件，指定 --kns 则无需指定 -k,-n,-s')
+    parser.add_argument('-i', '--input', help='输入文件，可根据文件后缀格式类型读取，txt是tab分隔')
+    parser.add_argument('--input-header',
+                        help="默认 GeneID 添加定义，如果有其他列名，请写出列名，如果没有列名，输入列的位置，从 0 开始数，列名不可以是数字")
     parser.add_argument('--kns', help='输入 kns_def 文件，添加 KEGG_ID, KEGG_GeneID, NR_Def, Swiss_protein_ID')
+    parser.add_argument('-c', '--specify-column', help='指定 kns 根据哪一列进行合并，针对需要对 GeneSymbol 列进行合并优化')
     parser.add_argument('-k', '--kegg', help='KEGG_gene_def 文件，会添加 KEGG_ID 和 KEGG_Shortname 列')
     parser.add_argument('-n', '--nr', help='NR_gene_def 文件')
     parser.add_argument('-s', '--swiss', help='Swiss_gene_def 文件')
-    parser.add_argument('-i', '--input', help='输入文件，可根据文件后缀格式类型读取，txt是tab分隔')
-    parser.add_argument('--input-header', default='GeneID', dest='input_header',
-                        help="默认 GeneID 添加定义，如果有其他列名，请写出列名，如果没有列名，输入列的位置，从 0 开始数，列名不可以是数字")
     parser.add_argument('-o', '--output', default='output.txt', help='输出文件')
-    parser.add_argument('--merge-how', default='left', dest='merge_how', help='合并方式，默认 left')
+    parser.add_argument('--merge-how', default='left', help='合并方式，默认 left')
     
     add_expression = parser.add_argument_group(title="指定添加 expression 的参数")
     add_expression.add_argument('-e', '--expression', type=str, help='表达量文件，通常是 fpkm_reads_matrix_data_def.txt')
@@ -30,16 +31,20 @@ def parse_input():
     args = parser.parse_args()
     
     if args.kns:
-        args.kegg, args.nr, args.swiss = None, None, None
+        args.kegg = args.nr = args.swiss = None
+
+    if not args.input_header and not args.specify_column:
+        args.input_header = args.specify_column = 'GeneID'
+    elif not args.input_header:
+        args.input_header = args.specify_column
+    elif not args.specify_column:
+        args.specify_column = 'GeneID'
     
     return args
 
 
-def add_kns_def(file_df, kegg_file=None, nr_file=None, swiss_file=None, kns_file=None):
-    """
-    传入表中必须包含 GeneID 列
-    添加 NR_Def, Swiss_protein_ID, KEGG_ID, KEGG_Shortname, EC_number, KEGG_Description 列
-    """
+def add_kns_def(file_df, for_merge_column, kegg_file=None, nr_file=None, swiss_file=None, kns_file=None):
+    """对输入表添加基因定义"""
     result_df = file_df.copy()
     source_shape = file_df.shape[0]
 
@@ -77,16 +82,19 @@ def add_kns_def(file_df, kegg_file=None, nr_file=None, swiss_file=None, kns_file
         result_df = pd.merge(left=result_df, right=kegg_df, on='GeneID', how='left')
 
     if kns_file:
-        kns_df = load_table(kns_file, dtype={'GeneID': 'str'})
-        result_df = pd.merge(left=result_df, right=kns_df, on='GeneID', how='left')
+        kns_df = load_table(kns_file, dtype={for_merge_column: 'str'})
+        duplicate_cols = [col for col in kns_df.columns if col in result_df.columns and col != for_merge_column]
+        if duplicate_cols:
+            kns_df = kns_df.drop(columns=duplicate_cols)
+        result_df = pd.merge(left=result_df, right=kns_df, on=for_merge_column, how='left')
 
     diff_col = list(set(result_df.columns) - set(file_df.columns))
     result_df[diff_col] = result_df[diff_col].fillna(value='N/A')
     result_shape = result_df.shape[0]
     
     if source_shape != result_shape:
-        logger.info(f"原表行数{source_shape}, 结果表行数{result_shape}, 可能输入文件基因 ID 有重复，或注释文件有重复")
-        result_df.drop_duplicates(subset='GeneID', inplace=True)
+        logger.info(f"原表行数{source_shape}, 结果表行数{result_shape}, 可能输入文件指定合并列有重复，或注释文件有重复")
+        result_df.drop_duplicates(subset=for_merge_column, inplace=True)
         logger.info('已自动去重处理')
 
     return result_df
@@ -106,33 +114,36 @@ def add_expression_data(input_file_df, expression_data, merge_how):
 
 def main():
     args = parse_input()
+    for_merge_column = args.specify_column
     
     try:
         args.input_header = int(args.input_header)
     except ValueError:
         pass
 
-    if type(args.input_header) == str:
+    if isinstance(args.input_header, str):
         df = load_table(args.input)
-    elif type(args.input_header) == int:
+    elif isinstance(args.input_header, int):
         df = load_table(args.input, header=None)
     else:
         logger.error('输入的 input_header 有误，请检查')
         exit(1)
-    
-    df.rename(columns={args.input_header: 'GeneID'}, inplace=True)
-    df['GeneID'] = df["GeneID"].astype(str)
+
+    if args.input_header != for_merge_column:
+        df.rename(columns={args.input_header: for_merge_column}, inplace=True)
+    logger.debug(f'{df.columns}')
+    df[for_merge_column] = df[for_merge_column].astype(str)
     
     if args.expression:
         result_df = add_expression_data(df, args.expression, args.merge_how)
     else:
-        result_df = add_kns_def(df, args.kegg, args.nr, args.swiss, args.kns)
+        result_df = add_kns_def(df, for_merge_column, args.kegg, args.nr, args.swiss, args.kns)
     
     if args.output.endswith('.xlsx'):
-        result_df = result_df.rename(columns={'GeneID': args.input_header})
+        result_df = result_df.rename(columns={for_merge_column: args.input_header})
         result_df.to_excel(args.output, index=False, engine='openpyxl')
     if type(args.input_header) == str:
-        result_df = result_df.rename(columns={'GeneID': args.input_header})
+        result_df = result_df.rename(columns={for_merge_column: args.input_header})
         result_df.to_csv(args.output, sep='\t', index=False)
     elif type(args.input_header) == int:
         result_df.to_csv(args.output, sep='\t', index=False, header=True)
