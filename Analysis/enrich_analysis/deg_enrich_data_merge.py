5f87effaed20feab1d11872a99252f99f70d3867
WilliamGoGo <realwilliamchin@outlook.com>
2025-07-07 16:04:00 +0800

    [REFACTOR] Analysis/MultiDESeq/multideseq.py --> Analysis/MultiDESeq/deg_multideseq.py 添加 deg enrich 合并代码，添加 deg summary 画图代码， Analysis/enrich_analysis/deg_enrich_data_merge.py 优化 data merge

--- Analysis/enrich_analysis/deg_enrich_data_merge.py
+++ Analysis/enrich_analysis/deg_enrich_data_merge.py
@@ -14,42 +14,41 @@ from load_input import load_table
 def parse_input():
     p = argparse.ArgumentParser()
     p.add_argument('-i', '--input-dir', default=os.getcwd(), help='input data dir')
-    p.add_argument('-c', '--compare-info',  help='input compare_info.txt file')
+    p.add_argument('-c', '--compare-file',  help='input compare_info.txt file')
     p.add_argument('-o', '--output-file', default='DEG_enrichment_significant_pathway_summary.xlsx',
                    help='output file endswith .xlsx, GO in sheet1, KEGG in sheet2')
     
     return p.parse_args()
 
 
-def main():
-    args = parse_input()
+def deg_enrich_data_merge(input_dir, comp_file, output_file):
     go_enrich_df_down_list = []
     go_enrich_df_up_list = []
     kegg_enrich_df_down_list = []
     kegg_enrich_df_up_list = []
     
-    compare_df = load_table(args.compare_info)
+    compare_df = load_table(comp_file)
     for i, compare in compare_df.iterrows():
         compare_info = compare['Treat'] + "-vs-" + compare['Control']
 
-        for enrich_file in os.listdir(args.input_dir):
+        for enrich_file in os.listdir(input_dir):
             if enrich_file.startswith(compare_info) and enrich_file.endswith('Down_EnrichmentGO.xlsx'):
-                go_enrich_df_down = load_table(os.path.join(args.input_dir, enrich_file))
+                go_enrich_df_down = load_table(os.path.join(input_dir, enrich_file))
                 go_enrich_df_down['Comparison_ID'] = compare_info
                 go_enrich_df_down['Regulation'] = 'Down'
                 go_enrich_df_down_list.append(go_enrich_df_down)
             elif enrich_file.startswith(compare_info) and enrich_file.endswith('Up_EnrichmentGO.xlsx'):
-                go_enrich_df_up = load_table(os.path.join(args.input_dir, enrich_file))
+                go_enrich_df_up = load_table(os.path.join(input_dir, enrich_file))
                 go_enrich_df_up['Comparison_ID'] = compare_info
                 go_enrich_df_up['Regulation'] = 'Up'
                 go_enrich_df_up_list.append(go_enrich_df_up)
             elif enrich_file.startswith(compare_info) and enrich_file.endswith('Down_EnrichmentKEGG.xlsx'):
-                kegg_enrich_df_down = load_table(os.path.join(args.input_dir, enrich_file))
+                kegg_enrich_df_down = load_table(os.path.join(input_dir, enrich_file))
                 kegg_enrich_df_down['Comparison_ID'] = compare_info
                 kegg_enrich_df_down['Regulation'] = 'Down'
                 kegg_enrich_df_down_list.append(kegg_enrich_df_down)
             elif enrich_file.startswith(compare_info) and enrich_file.endswith('Up_EnrichmentKEGG.xlsx'):
-                kegg_enrich_df_up = load_table(os.path.join(args.input_dir, enrich_file))
+                kegg_enrich_df_up = load_table(os.path.join(input_dir, enrich_file))
                 kegg_enrich_df_up['Comparison_ID'] = compare_info
                 kegg_enrich_df_up['Regulation'] = 'Up'
                 kegg_enrich_df_up_list.append(kegg_enrich_df_up)
@@ -82,10 +81,16 @@ def main():
         go_summary = pd.concat([go_down_summary, go_up_summary])
         kegg_summary = pd.concat([kegg_down_summary, kegg_up_summary])
         
-        with pd.ExcelWriter(args.output_file, engine='openpyxl') as w:
+        with pd.ExcelWriter(output_file, engine='openpyxl') as w:
             go_summary.to_excel(w, sheet_name='GO_analysis', index=False)
             kegg_summary.to_excel(w, sheet_name='KEGG_analysis', index=False)
     
+
+
+def main():
+    args = parse_input()
+    deg_enrich_data_merge(args.input_dir, args.compare_file, args.output_file)
+
 
 if __name__ == '__main__':
     main()
