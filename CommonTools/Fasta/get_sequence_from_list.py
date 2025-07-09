fe4da7ac4595eee19604a88719df4a4d4a3e7662
WilliamGoGo <realwilliamchin@outlook.com>
2025-07-09 13:07:17 +0800

    [UPDATE] CommonTools/Fasta/get_sequence_from_list.py 优化

--- CommonTools/Fasta/get_sequence_from_list.py
+++ CommonTools/Fasta/get_sequence_from_list.py
@@ -5,6 +5,7 @@ import pandas as pd
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from loguru import logger
+from collections import defaultdict
 
 
 def parse_input():
@@ -24,8 +25,10 @@ def get_seq_from_idlist(id_df, fasta, save_type, output):
     has_strand = 'Strand' in id_df.columns
     has_targetid = 'TargetGeneID' in id_df.columns
     only_geneid = 'GeneID' in id_df.columns and 'TargetGeneID' not in id_df.columns and 'Start' not in id_df.columns and 'End' not in id_df.columns
-    # 读取原始fasta文件并将序列存储到字典中
-    fasta_sequences = {record.id: record for record in SeqIO.parse(fasta, 'fasta')}
+    # 读取原始fasta文件并将序列存储到字典中（支持同名id全部输出）
+    fasta_sequences = defaultdict(list)
+    for record in SeqIO.parse(fasta, 'fasta'):
+        fasta_sequences[record.id].append(record)
 
     sequences = []
     for idx, row in id_df.iterrows():
@@ -36,41 +39,34 @@ def get_seq_from_idlist(id_df, fasta, save_type, output):
                 start, end = end, start
             start = start - 1
             if row['GeneID'] in fasta_sequences:
-                full_sequence = fasta_sequences[row['GeneID']]
-                sub_sequence = full_sequence.seq[start:end]
-                if has_strand and row['Strand'] == '-':
-                    sub_sequence = sub_sequence.reverse_complement()
-                if has_targetid:
-                    new_record = SeqRecord(sub_sequence, id=f"{row['TargetGeneID']}", name='', description='')
-                else:
-                    new_record = SeqRecord(sub_sequence, id=row['GeneID'], name='', description='')
-                sequences.append(new_record)
+                for full_sequence in fasta_sequences[row['GeneID']]:
+                    sub_sequence = full_sequence.seq[start:end]
+                    if has_strand and row['Strand'] == '-':
+                        sub_sequence = sub_sequence.reverse_complement()
+                    if has_targetid:
+                        new_record = SeqRecord(sub_sequence, id=f"{row['TargetGeneID']}", name='', description='')
+                    else:
+                        new_record = SeqRecord(sub_sequence, id=row['GeneID'], name='', description='')
+                    sequences.append(new_record)
 
         elif only_geneid:
             if save_type == 'on':
                 if row['GeneID'] in fasta_sequences:
-                    sequences.append(fasta_sequences[row['GeneID']])
+                    for rec in fasta_sequences[row['GeneID']]:
+                        sequences.append(rec)
 
         elif has_targetid:
-                
-            # id_df = id_df.set_index('GeneID')
             if row['GeneID'] in fasta_sequences:
-                new_record = SeqRecord(fasta_sequences[row['GeneID']].seq, id=row['TargetGeneID'], name='', description='')
-                # cur_seq_id = seq.id
-                # # target_id = id_df.loc[cur_seq_id]['TargetGeneID']
-                # target_id = id_df[id_df['GeneID'] == cur_seq_id]['TargetGeneID'].values[0]
-                # seq.id = target_id
-                # seq.name = ''
-                # seq.description = ''
-                sequences.append(new_record)
-            # SeqIO.write(new_sequences, output, 'fasta')
-            # sys.exit(0)
+                for rec in fasta_sequences[row['GeneID']]:
+                    new_record = SeqRecord(rec.seq, id=row['TargetGeneID'], name='', description='')
+                    sequences.append(new_record)
     if only_geneid and save_type == 'off':
         # 获取所有不在id_df中的GeneID
         all_gene_ids = set(fasta_sequences.keys())
         target_gene_ids = set(id_df['GeneID'])
         for gene_id in all_gene_ids - target_gene_ids:
-            sequences.append(fasta_sequences[gene_id])
+            for rec in fasta_sequences[gene_id]:
+                sequences.append(rec)
 
     SeqIO.write(sequences, output, 'fasta')
 
