4c08d1a4dd4a1544815dcc91c75a455d9127c6a8
WilliamGoGo <realwilliamchin@outlook.com>
2025-07-14 14:06:15 +0800

    [NEW FEATURE] transcriptome/target_gene_analysis_heatmap.py 每个 DEG 的 Ontology 也画一张 heatmap 图，参数优化，其他代码结构优化

--- transcriptome/target_gene_analysis_heatmap.py
+++ transcriptome/target_gene_analysis_heatmap.py
@@ -22,58 +22,93 @@ from load_input import load_table, write_output_df
 def parse_input():
     parser = argparse.ArgumentParser()
     # 输入文件
-    parser.add_argument('-i', '--targetgene', dest="target_gene_file", type=str, required=True,
-                           help='输入文件，target gene 文件，至少包含两列，GeneID 和 Ontology')
-    parser.add_argument('--fpkm', type=str, required=True, help='输入 fpkm_matrix.txt 文件')
-    parser.add_argument('--kns', type=str, help='anova_p 文件添加定义输出')
-    parser.add_argument('--deg-data-dir', dest='deg_data_dir', type=str, help='[必须]输入 DEG_data.txt 文件')
-    parser.add_argument('-s', '--samplesinfo', type=str, required=True,
-                        help='输入样品信息文件')
+    parser.add_argument('-i', '--target-gene-file', required=True,
+                        help='输入文件，target gene 文件，至少包含三列，GeneID 和 Ontology, SubOntology')
+    parser.add_argument('--fpkm', required=True, help='输入 fpkm_matrix.txt 文件')
+    parser.add_argument('-d', '--deg-data-dir', help='[必须]输入 DEG_data.txt 文件')
+    parser.add_argument('-s', '--samplesinfo', required=True, help='输入样品信息文件')
     # 运行参数
     parser.add_argument('--mean', default=False, type=bool, help='使用每组中的平均数画 heatmap')
-    parser.add_argument('--specie-type', dest='specie_type',
-                        choices=['homo_sapiens', 'mus_musculus', 'other'], default='other',
-                        help='输入运行的物种类型，人，小鼠，或者其他，人小鼠使用 GeneSymbol 作为索引画图')
+    parser.add_argument('--genesymbol', action='store_true', help='使用 GeneSymbol 作为索引，人和小鼠通常使用')
     # 输出
-    parser.add_argument('-o', '--output', type=str, default=os.getcwd(), help='输出目录')
+    parser.add_argument('-o', '--output', default=os.getcwd(), help='输出目录')
     
     args = parser.parse_args()
 
     return args
 
 
-def group_vs_group_heatmap(group_target_gene_file, samples_file, specie_type, output_dir):
-    df = load_table(group_target_gene_file, dtype={"GeneID": str})
-    if specie_type in ['homo_sapiens', 'mus_musculus']:
-        index_col = 'GeneSymbol'
-    else:
-        index_col = 'GeneID'
-    df.dropna(subset=index_col, inplace=True)
-    fpkm_df = df[[index_col] + [col for col in df.columns if col.endswith('FPKM')]]
-    no_FPKM_str_columns = [col.replace('_FPKM', '') for col in fpkm_df.columns[1:]]
-    fpkm_df.columns = [index_col] + no_FPKM_str_columns
-    ontology_df = df[[index_col, 'SubOntology', 'Ontology']]
+def genesymbol_data_pre_process(df: pd.DataFrame) -> pd.DataFrame:
+    # 如果 GeneSymbol 有 NA 则会去除一些
+    df_source_lines_num = df.shape[0]
+    df.dropna(subset='GeneSymbol', inplace=True)
+    df['GeneSymbol'] = df['GeneSymbol'].replace(['NA', 'N/A', '-', '---', 'na', 'n/a', ''], np.nan)
+    df_dropedna_lines_num = df.shape[0]
+    df.drop_duplicates(subset='GeneSymbol', inplace=True)
+    df_droped_duplicates_lines_num = df.shape[0]
+    if df_dropedna_lines_num != df_droped_duplicates_lines_num:
+        logger.info(f'对输入表预处理之前为 {df_source_lines_num}')
+        logger.info(f'去 NA 之后为 {df_dropedna_lines_num}')
+        logger.info(f'去重复之后为 {df_droped_duplicates_lines_num}')
+        logger.warning(f'输入文件有 GeneSymbol 为空或重复，将会去重再分析画图')
     
-    os.makedirs(os.path.join(output_dir, 'Prep_files'), exist_ok=True)
-    group_vs_group_heatmap_fname = os.path.join(
-        output_dir,
-        'Prep_files',
-        os.path.basename(group_target_gene_file).replace('_data.txt', '_heatmap.xlsx')
-    )
-    group_vs_group_heatmap_pname = group_target_gene_file.replace('_data.txt', '_heatmap.jpeg')
-    
-    group1 = df.head(1)['sampleA'].values[0]
-    group2 = df.head(1)['sampleB'].values[0]
-    samples_df = pd.read_csv(samples_file, sep='\t', usecols=[0, 1])
+    return df
+
+
+def apply_group_mean(df: pd.DataFrame, samples_df: pd.DataFrame):
+    """
+    对 df 按 samples_df 的 group 分组取均值，并调整 samples_df 结构。
+    返回新的 df 和 samples_df。
+    """
+    for each_group in samples_df['group'].unique():
+        group_samples = samples_df[samples_df['group'] == each_group]['sample']
+        df[each_group] = df[group_samples].mean(axis=1)
+    df = df.drop(columns=samples_df['sample'])
+    samples_df = samples_df.drop(columns=['sample']).drop_duplicates(subset=['group'])
+    samples_df['sample'] = samples_df['group']
     samples_df = samples_df[['sample', 'group']]
-    samples_df = samples_df[samples_df['group'].isin([group1, group2])]
+    return df, samples_df
+
+
+def target_gene_heatmap(target_gene_df, fpkm_matrix_df, samples_df, index_col, group_mean, output_pic):
+    sample_columns = samples_df['sample'].tolist()
     
-    with pd.ExcelWriter(group_vs_group_heatmap_fname, engine='openpyxl') as writer:
-        fpkm_df.to_excel(writer, sheet_name='Sheet1', index=False)
+    # 目标基因添加 fpkm 值，准备画图文件
+    gene_fpkm_df = pd.merge(target_gene_df, fpkm_matrix_df, on='GeneID', how='inner')
+    gene_fpkm_df.sort_values(by=['Ontology', 'SubOntology'], inplace=True)
+    
+    # 先画全部基因的热图（原有功能）
+    all_gene_heatmap_filename = output_pic.replace('.jpg', '.xlsx')
+    # multigroup_heatmap_data_df = gene_fpkm_df.loc[:, (gene_fpkm_df.columns != 'Ontology') & (gene_fpkm_df.columns != 'SubOntology')].copy()
+    multigroup_heatmap_data_df = gene_fpkm_df[[index_col] + sample_columns].copy()
+    multigroup_heatmap_sheet3_df = gene_fpkm_df[[index_col, 'SubOntology', 'Ontology']].copy()
+    
+    # if group mean 根据 samplesinfo 每组中的平均数画 heatmap
+    if group_mean:
+        multigroup_heatmap_data_df, samples_df = apply_group_mean(multigroup_heatmap_data_df, samples_df)
+
+    with pd.ExcelWriter(all_gene_heatmap_filename, engine='openpyxl') as writer:
+        multigroup_heatmap_data_df.to_excel(writer, sheet_name="Sheet1", index=False)
         samples_df.to_excel(writer, sheet_name='Sheet2', index=False)
-        ontology_df.to_excel(writer, sheet_name='Sheet3', index=False)
-    
-    draw_multigroup_heatmap(group_vs_group_heatmap_fname, group_vs_group_heatmap_pname, other_args='--no-cluster-rows')
+        multigroup_heatmap_sheet3_df.to_excel(writer, sheet_name='Sheet3', index=False)
+    draw_multigroup_heatmap(all_gene_heatmap_filename, output_pic, other_args='--no-cluster-rows')
+
+    # 每个 Ontology 单独画图
+    for ontology, sub_df in gene_fpkm_df.groupby('Ontology'):
+        heatmap_data_df = sub_df[['GeneSymbol'] + sample_columns].copy()
+        heatmap_sheet3_df = sub_df[['GeneSymbol', 'SubOntology', 'Ontology']].copy()
+        
+        # if group mean 根据 samplesinfo 每组中的平均数画 heatmap
+        if group_mean:
+            heatmap_data_df, sub_samples_df = apply_group_mean(heatmap_data_df, sub_samples_df)
+        
+        ontology_excel = all_gene_heatmap_filename.replace('.xlsx', f'_{ontology}.xlsx')
+        ontology_pic = output_pic.replace('.jpeg', f'_{ontology}.jpeg')
+        with pd.ExcelWriter(ontology_excel, engine='openpyxl') as writer:
+            heatmap_data_df.to_excel(writer, sheet_name="Sheet1", index=False)
+            samples_df.to_excel(writer, sheet_name='Sheet2', index=False)
+            heatmap_sheet3_df.to_excel(writer, sheet_name='Sheet3', index=False)
+        draw_multigroup_heatmap(ontology_excel, ontology_pic, other_args='--no-cluster-rows')
 
 
 def deg_target_gene_summary(target_gene_data_list):
@@ -102,83 +137,71 @@ def deg_target_gene_summary(target_gene_data_list):
     return summary_df
 
 
-def genesymbol_data_pre_process(df: pd.DataFrame) -> pd.DataFrame:
-    # 如果 GeneSymbol 有 NA 则会去除一些
-    df_source_lines_num = df.shape[0]
-    df.dropna(subset='GeneSymbol', inplace=True)
-    df['GeneSymbol'] = df['GeneSymbol'].replace(['NA', 'N/A', '-', '---', 'na', 'n/a', ''], np.nan)
-    df_dropedna_lines_num = df.shape[0]
-    df.drop_duplicates(subset='GeneSymbol', inplace=True)
-    df_droped_duplicates_lines_num = df.shape[0]
-    if df_dropedna_lines_num != df_droped_duplicates_lines_num:
-        logger.info(f'对输入表预处理之前为 {df_source_lines_num}')
-        logger.info(f'去 NA 之后为 {df_dropedna_lines_num}')
-        logger.info(f'去重复之后为 {df_droped_duplicates_lines_num}')
-        logger.warning(f'输入文件有 GeneSymbol 为空或重复，将会去重再分析画图')
-    
-    return df
+def deg_target_gene_heatmap(target_gene_def_df, samples_df, deg_data_dir, index_col, output_dir):
+    result_target_gene_data_list = []
+    samples_df = samples_df[['sample', 'group']]
+    deg_data_list = [x for x in os.listdir(deg_data_dir) if x.endswith('_DEG_data.txt')]
+    for deg_data_file in deg_data_list:
+        comparsion_name = os.path.basename(deg_data_file).replace('_DEG_data.txt', '')
+        logger.info(f'正在画 {comparsion_name} heatmap')
+        
+        group1 = comparsion_name.split('-vs-')[0]
+        group2 = comparsion_name.split('-vs-')[1]
+        comparsion_samples_df = samples_df[samples_df['group'].isin([group1, group2])]
+        comparsion_samples_list = comparsion_samples_df['sample'].values.tolist()
+        comparsion_dir = os.path.join(output_dir, comparsion_name)
+        os.makedirs(comparsion_dir, exist_ok=True)
+        
+        logger.info(f'正在找相关基因添加定义 {comparsion_name}')
+        deg_data_file = os.path.join(deg_data_dir, deg_data_file)
+        deg_data_df = load_table(deg_data_file, dtype={'GeneID': str})
+        
+        # 目标基因和 DEG_data 合并，文件预处理
+        comparsion_target_gene_df = pd.merge(target_gene_def_df, right=deg_data_df, on=index_col, how='inner', suffixes=('_df1', '_df2'))
+        comparsion_target_gene_df.dropna(subset=index_col, inplace=True)  # 去掉 NA 行
+        cols_to_drop = [col for col in comparsion_target_gene_df.columns if col.endswith('_df1')]
+        comparsion_target_gene_df.drop(columns=cols_to_drop, inplace=True)
+        comparsion_target_gene_df.columns = [col.replace('_df2', '') for col in comparsion_target_gene_df.columns]
+        comparsion_target_gene_df.columns = [col[:-5] if col.endswith('_FPKM') else col for col in comparsion_target_gene_df.columns]
+        comparsion_target_gene_file = os.path.join(comparsion_dir, f'{comparsion_name}_target_gene_data.xlsx')
+        write_output_df(comparsion_target_gene_df, comparsion_target_gene_file, index=False)
+        result_target_gene_data_list.append(comparsion_target_gene_file)
+        
+        fpkm_df = comparsion_target_gene_df[[index_col] + comparsion_samples_list]
+        ontology_df = comparsion_target_gene_df[[index_col, 'SubOntology', 'Ontology']]
+        
+        group_vs_group_heatmap_fname = os.path.join(comparsion_dir, f'{comparsion_name}_target_gene_heatmap.xlsx')
+        group_vs_group_heatmap_pname = os.path.join(comparsion_dir, f'{comparsion_name}_target_gene_heatmap.jpg')
 
+        with pd.ExcelWriter(group_vs_group_heatmap_fname, engine='openpyxl') as writer:
+            fpkm_df.to_excel(writer, sheet_name='Sheet1', index=False)
+            comparsion_samples_df.to_excel(writer, sheet_name='Sheet2', index=False)
+            ontology_df.to_excel(writer, sheet_name='Sheet3', index=False)
+        
+        draw_multigroup_heatmap(group_vs_group_heatmap_fname, group_vs_group_heatmap_pname, other_args='--no-cluster-rows')
+        
+        # 每个组间的每个 Ontology 画一张图
+        for ontology, sub_df in comparsion_target_gene_df.groupby('Ontology'):
+            logger.info(f'正在画 {comparsion_name} {ontology} heatmap')
+            ontology_heatmap_data_df = sub_df[[index_col] + comparsion_samples_list].copy()
+            ontology_def_df = sub_df[[index_col, 'SubOntology', 'Ontology']].copy()
 
-def target_gene_heatmap(target_gene_df, fpkm_matrix_df, samples_df, specie_type, group_mean, output_pic):
-    sample_columns = samples_df['sample'].tolist()
-    
-    # 目标基因添加 fpkm 值，准备画图文件
-    gene_fpkm_df = pd.merge(target_gene_df, fpkm_matrix_df, on='GeneID', how='inner')
-    gene_fpkm_df.sort_values(by=['Ontology', 'SubOntology'], inplace=True)
-    
-    # 先画全部基因的热图（原有功能）
-    all_gene_heatmap_filename = output_pic.replace('.jpeg', '.xlsx')
-    # multigroup_heatmap_data_df = gene_fpkm_df.loc[:, (gene_fpkm_df.columns != 'Ontology') & (gene_fpkm_df.columns != 'SubOntology')].copy()
-    if specie_type in ['homo_sapiens', 'mus_musculus']:
-        multigroup_heatmap_data_df = gene_fpkm_df[['GeneSymbol'] + sample_columns].copy()
-        multigroup_heatmap_sheet3_df = gene_fpkm_df[['GeneSymbol', 'SubOntology', 'Ontology']].copy()
+            ontology_excel_name = os.path.join(comparsion_dir, f'{comparsion_name}_{ontology}_heatmap.xlsx')
+            ontology_pic_name = os.path.join(comparsion_dir, f'{comparsion_name}_{ontology}_heatmap.jpg')
+            with pd.ExcelWriter(ontology_excel_name, engine='openpyxl') as writer:
+                ontology_heatmap_data_df.to_excel(writer, sheet_name="Sheet1", index=False)
+                comparsion_samples_df.to_excel(writer, sheet_name="Sheet2", index=False)
+                ontology_def_df.to_excel(writer, sheet_name="Sheet3", index=False)
+
+            draw_multigroup_heatmap(ontology_excel_name, ontology_pic_name, other_args='--no-cluster-rows')
+            
+    # 超过两组比较汇总结果，单个没有意义
+    if len(result_target_gene_data_list) >= 2:
+        logger.info('正在对结果汇总')
+        target_gene_summary_df = deg_target_gene_summary(result_target_gene_data_list)
+        write_output_df(target_gene_summary_df, os.path.join(output_dir, 'Target_gene_summary_data.txt'), index=False)
     else:
-        multigroup_heatmap_data_df = gene_fpkm_df[['GeneID'] + sample_columns].copy()
-        multigroup_heatmap_sheet3_df = gene_fpkm_df[['GeneID', 'SubOntology', 'Ontology']].copy()
-    
-    # 根据 samplesinfo 每组中的平均数画 heatmap
-    if group_mean:
-        for each_group in samples_df['group'].unique():
-            group_samples = samples_df[samples_df['group'] == each_group]['sample']
-            multigroup_heatmap_data_df[each_group] = multigroup_heatmap_data_df[group_samples].mean(axis=1)
-        multigroup_heatmap_data_df.drop(columns=samples_df['sample'], inplace=True)
-        samples_df = samples_df.drop(columns=['sample']).drop_duplicates(subset=['group'])
-        samples_df['sample'] = samples_df['group']
-        samples_df = samples_df[['sample', 'group']]
-
-    with pd.ExcelWriter(all_gene_heatmap_filename, engine='openpyxl') as writer:
-        multigroup_heatmap_data_df.to_excel(writer, sheet_name="Sheet1", index=False)
-        samples_df.to_excel(writer, sheet_name='Sheet2', index=False)
-        multigroup_heatmap_sheet3_df.to_excel(writer, sheet_name='Sheet3', index=False)
-    draw_multigroup_heatmap(all_gene_heatmap_filename, output_pic, other_args='--no-cluster-rows')
-
-    # 每个 Ontology 单独画图
-    for ontology, sub_df in gene_fpkm_df.groupby('Ontology'):
-        if specie_type in ['homo_sapiens', 'mus_musculus']:
-            sub_df = sub_df.dropna(subset=['GeneSymbol'])
-            heatmap_data_df = sub_df[['GeneSymbol'] + sample_columns].copy()
-            heatmap_sheet3_df = sub_df[['GeneSymbol', 'SubOntology', 'Ontology']].copy()
-        else:
-            heatmap_data_df = sub_df[['GeneID'] + sample_columns].copy()
-            heatmap_sheet3_df = sub_df[['GeneID', 'SubOntology', 'Ontology']].copy()
-        sub_samples_df = samples_df.copy()
-        if group_mean:
-            for each_group in sub_samples_df['group'].unique():
-                group_samples = sub_samples_df[sub_samples_df['group'] == each_group]['sample']
-                heatmap_data_df[each_group] = heatmap_data_df[group_samples].mean(axis=1)
-            heatmap_data_df.drop(columns=sub_samples_df['sample'], inplace=True)
-            sub_samples_df = sub_samples_df.drop(columns=['sample']).drop_duplicates(subset=['group'])
-            sub_samples_df['sample'] = sub_samples_df['group']
-            sub_samples_df = sub_samples_df[['sample', 'group']]
-        # 处理文件名中的非法字符
-        safe_ontology = str(ontology).replace('/', '_').replace(' ', '_')
-        ontology_excel = all_gene_heatmap_filename.replace('.xlsx', f'_{safe_ontology}.xlsx')
-        ontology_pic = output_pic.replace('.jpeg', f'_{safe_ontology}.jpeg')
-        with pd.ExcelWriter(ontology_excel, engine='openpyxl') as writer:
-            heatmap_data_df.to_excel(writer, sheet_name="Sheet1", index=False)
-            sub_samples_df.to_excel(writer, sheet_name='Sheet2', index=False)
-            heatmap_sheet3_df.to_excel(writer, sheet_name='Sheet3', index=False)
-        draw_multigroup_heatmap(ontology_excel, ontology_pic, other_args='--no-cluster-rows')
+        logger.info('跳过结果汇总，比较组少于 2 个')
 
 
 def main():
@@ -190,49 +213,23 @@ def main():
     # 加载为 DataFrame
     samples_df = load_table(samples_file, usecols=[0, 1], dtype=str)
     target_gene_def_df = load_table(target_gene_file, dtype={'GeneID': str})
+    target_gene_def_df['Ontology'] = target_gene_def_df['Ontology'].str.replace('/', '_').str.replace(' ', '_')
     
     # 对文件预处理
     samples_df = samples_df[['sample', 'group']]
     target_gene_def_df = df_drop_element_side_space(target_gene_def_df)
-    if args.specie_type.lower() in ['homo_sapiens', 'mus_musculus']:
+    if args.genesymbol:
+        index_col = 'GeneSymbol'
         target_gene_def_df = genesymbol_data_pre_process(target_gene_def_df)
+    else:
+        index_col = 'GeneID'
         
     fpkm_matrix_df = load_table(fpkm_matrix_file, dtype={'GeneID': str})
     fpkm_matrix_df = df_drop_row_sum_eq_zero(fpkm_matrix_df)
     
-    all_gene_heatmap_pic = os.path.join(output_dir, target_gene_file.split(os.sep)[-1].replace('.txt', '_heatmap.jpeg'))
-    target_gene_heatmap(target_gene_def_df, fpkm_matrix_df, samples_df, args.specie_type, args.mean, all_gene_heatmap_pic)
-
-    
-    result_target_gene_data_list = []
-    if args.deg_data_dir:
-        deg_data_list = [x for x in os.listdir(args.deg_data_dir) if x.endswith('_DEG_data.txt')]
-        for deg_data_file in deg_data_list:
-            compare_name = os.path.basename(deg_data_file).replace('_DEG_data.txt', '')
-            logger.info(f'正在找相关基因添加定义 {compare_name}')
-            deg_data_file = os.path.join(args.deg_data_dir, deg_data_file)
-            deg_data_df = load_table(deg_data_file, dtype={'GeneID': str})
-
-            # result_df = pd.merge(deg_data_df, right=target_gene_def_df, on='GeneID', how='left', suffixes=('_df1', '_df2'))
-            result_df = pd.merge(target_gene_def_df, right=deg_data_df, on='GeneID', how='inner', suffixes=('_df1', '_df2'))
-            cols_to_drop = [col for col in result_df.columns if col.endswith('_df1')]
-            result_df.drop(columns=cols_to_drop, inplace=True)
-            result_df.columns = [col.replace('_df2', '') for col in result_df.columns]
-            result_df.to_csv(os.path.join(args.output, f'{compare_name}_target_gene_data.txt'), sep='\t', index=False)
-            result_target_gene_data_list.append(os.path.join(args.output, f'{compare_name}_target_gene_data.txt'))
-            
-            logger.info(f'正在画 {compare_name} heatmap')
-            group_vs_group_heatmap(
-                os.path.join(args.output, f'{compare_name}_target_gene_data.txt'),
-                samples_file,
-                args.specie_type,
-                args.output
-            )
-    
-    if len(result_target_gene_data_list) > 0:
-        logger.info('正在对结果汇总')
-        target_gene_summary_df = deg_target_gene_summary(result_target_gene_data_list)
-        write_output_df(target_gene_summary_df, os.path.join(args.output, 'Target_gene_summary_data.txt'), index=False)
+    all_gene_heatmap_pic = os.path.join(output_dir, target_gene_file.split(os.sep)[-1].replace('.txt', '_heatmap.jpg'))
+    target_gene_heatmap(target_gene_def_df, fpkm_matrix_df, samples_df, index_col, args.mean, all_gene_heatmap_pic)
+    deg_target_gene_heatmap(target_gene_def_df, samples_df, args.deg_data_dir, index_col, args.output)
     
     logger.success("Done")
 
