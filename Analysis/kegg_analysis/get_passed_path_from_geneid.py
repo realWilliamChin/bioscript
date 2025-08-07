f1c1d6e1f631f0cbefceaa33f837f3563376c482
WilliamGoGo <realwilliamchin@outlook.com>
2025-08-07 15:41:26 +0800

    [REFACTOR] Analysis/kegg_analysis/get_passed_path_from_geneid.py 重构代码

--- Analysis/kegg_analysis/get_passed_path_from_geneid.py
+++ Analysis/kegg_analysis/get_passed_path_from_geneid.py
@@ -7,11 +7,15 @@ import sys
 import pandas as pd
 import argparse
 
+sys.path.append('/home/colddata/qinqiang/script/CommonTools')
+from load_input import load_table, write_output_df
+
 
 def parse_input():
     p = argparse.ArgumentParser()
-    p.add_argument(dest='gene_file', help='输入第一列为 GeneID 的文件，需要有 GeneID 列名')
-    p.add_argument(dest='output_file', help='输出文件')
+    p.add_argument('-i', dest='gene_file', help='输入第一列为 GeneID 的文件，需要有 GeneID 列名')
+    p.add_argument('-o', dest='output_file', help='输出文件')
+    p.add_argument('--header', dest='header', action='store_true', help='输入文件是否有 GeneID header')
     p.add_argument('-k', '--keggclean', help='KEGG_clean 文件')
     
     args = p.parse_args()
@@ -19,15 +23,19 @@ def parse_input():
     return args
 
 
-def passed_path(gene_file, kegg_clean_file, output_file):
-    gene_df = pd.read_csv(gene_file, sep='\t')
-    kegg_df = pd.read_csv(kegg_clean_file, sep='\t', header=None, usecols=[0, 1, 4], names=['GeneID', 'KO_ID', 'KEGG_ID'])
+def main():
+    args = parse_input()
+    if args.header:
+        gene_df = load_table(args.gene_file)
+    else:
+        gene_df = load_table(args.gene_file, header=None, names=['GeneID'])
+    kegg_df = load_table(args.keggclean, header=None, usecols=[0, 1, 4], names=['GeneID', 'KO_ID', 'KEGG_ID'])
     kegg_df['KO_ID'] = kegg_df['KO_ID'].str.split(':').str[0]
     
     ko_def_df = pd.merge(gene_df, kegg_df, on='GeneID', how='left')
     regulation_df = ko_def_df.copy()
     regulation_df['regulation'] = '1'
-    regulation_df[['KEGG_ID', 'regulation']].drop_duplicates().dropna().to_csv('regulation.txt', sep='\t', index=False)
+    write_output_df(regulation_df[['KEGG_ID', 'regulation']].drop_duplicates().dropna(), 'regulation.txt', index=False)
     ko_def_df.drop(columns=['GeneID', 'KEGG_ID'], inplace=True)
     ko_def_df.drop_duplicates(subset=['KO_ID'], inplace=True)
     ko_def_df.dropna(inplace=True)
@@ -37,10 +45,8 @@ def passed_path(gene_file, kegg_clean_file, output_file):
     
     ko_def_df = passed_path_df[passed_path_df['KO_ID'].isin(ko_def_df['KO_ID'])]
     ko_def_df = ko_def_df.drop_duplicates(subset=['KO_ID'])
-    ko_def_df.to_csv(output_file, sep='\t', index=False, header=False)
+    write_output_df(ko_def_df, args.output_file, index=False, header=False)
 
 
 if __name__ == '__main__':
-    args = parse_input()
-    
-    passed_path(args.gene_file, args.keggclean, args.output_file)
+    main()
