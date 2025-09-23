6f04243943ed1761d72f3178cf351061f92a748c
WilliamGoGo <realwilliamchin@outlook.com>
2025-09-23 17:18:13 +0800

    [REFAcTOR] 16s/PY_creat_manifest_and_group_info_flash_joined.py 代码优化

--- 16s/PY_creat_manifest_and_group_info_flash_joined.py
+++ 16s/PY_creat_manifest_and_group_info_flash_joined.py
@@ -1,51 +1,91 @@
 import argparse
 import os
-from shutil import copyfile
 import sys
-parser=argparse.ArgumentParser(description="creat_manifest_txt:")
-parser.add_argument('--dataset_dir',dest='dataset_dir',default='input_data1',
-        help='path of the data')
-'''
-parser.add_argument('--continue_train',dest='continue_train',type=bool,default=False,
-        help='if continue training, load the latest model: 1: true, 0: false')
-'''
-parser.add_argument('--date_out',dest='data_out',default='output_data1',
-        help='path of output data')
-arg=parser.parse_args()
-if len(sys.argv)==1:
-    parser.print_help()
-    sys.exit(1)
+from pathlib import Path
+from loguru import logger
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Create manifest and group info files for 16S data")
+    parser.add_argument('--dataset_dir', dest='dataset_dir', default='input_data1',
+            help='Path of the input data directory')
+    parser.add_argument('--data_out', dest='data_out', default='output_data1',
+            help='Path of output data directory')
+    args = parser.parse_args()
+    
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    
+    return args
 
-inputPath=arg.dataset_dir
-outputPath=arg.data_out
-filelist=os.listdir(inputPath)
-f1=open(outputPath+os.sep+'manifest.txt','w+')
-f2=open(outputPath+os.sep+'group_info.tsv','w+')
-f1.write('sample-id,absolute-filepath,direction'+'\n')
-f2.write('sample-id'+'\t'+'class'+'\n')
-filelist.sort()
-path=os.getcwd()
-for file in filelist:
-    oldneed=file.split('_')
-    need=[]
-    for i in oldneed:
-        need+=i.split('.')
-    name1=need[0]
+def extract_class_name(name):
+    if not name:
+        logger.error("Empty sample name encountered")
+        return "unknown"
+    
+    try:
+        times = -1
+        while times >= -len(name) and name[times].isdecimal():
+            times -= 1
+        if times >= -len(name) and name[times] == '-':
+            times -= 1
+        return name[:times + 1]
+    except Exception as e:
+        logger.error(f"Error processing sample name '{name}': {str(e)}")
+        return "unknown"
 
-    times = -1
-    while name1[times].isdecimal():
-        times -= 1
-    if name1[times]=='-':
-        times-=1
-    classname = name1[:times + 1]
+def main():
+    args = parse_arguments()
+    
+    input_path = Path(args.dataset_dir)
+    output_path = Path(args.data_out)
+    
+    # 确保输出目录存在
+    output_path.mkdir(parents=True, exist_ok=True)
+    
+    if not input_path.exists():
+        logger.error(f"Input directory {input_path} does not exist")
+        sys.exit(1)
+    
+    try:
+        file_list = sorted(input_path.iterdir())
+        current_path = Path.cwd()
+        
+        manifest_path = output_path / 'manifest.txt'
+        group_info_path = output_path / 'group_info.tsv'
+        
+        with open(manifest_path, 'w') as f1, open(group_info_path, 'w') as f2:
+            f1.write('sample-id,absolute-filepath,direction\n')
+            f2.write('sample-id\tclass\n')
+            
+            for file_path in file_list:
+                if not file_path.is_file():
+                    continue
+                    
+                file_name = file_path.name
+                logger.info(f"Processing file: {file_name}")
+                
+                try:
+                    name_parts = file_name.split('_')
+                    name_parts = [part.split('.')[0] for part in name_parts]
+                    sample_id = name_parts[0]
+                    
+                    class_name = extract_class_name(sample_id)
+                    
+                    # 构建绝对路径
+                    abs_path = current_path / input_path / file_name
+                    
+                    f1.write(f'{sample_id},{abs_path},forward\n')
+                    f2.write(f'{sample_id}\t{class_name}\n')
+                except Exception as e:
+                    logger.error(f"Error processing file {file_name}: {str(e)}")
+                    continue
+                
+        logger.info(f"Successfully created manifest and group info files in {output_path}")
+        
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        sys.exit(1)
 
-
-    f1.write(name1+','+path+inputPath.split('.')[-1]+os.sep+file+','+'forward'+'\n')
-    f2.write(name1 + '\t' + classname + '\n')
-
-
-
-
-f1.close()
-f2.close()
+if __name__ == '__main__':
+    main()
