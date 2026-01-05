32470650c8134b946585f965cdc0b521d9e8a9f9
WilliamGoGo <realwilliamchin@outlook.com>
2026-01-05 17:20:39 +0800

    [REFACTOR] 16s/PY_flash_dumx_to_joined.py 重大重构和代码优化： 1. 重构参数解析，从 inputPath/outputPath 改为规范的 input-dir/output-dir/sample-info 参数 2. 移除复杂的文件名解析逻辑（splice、R 函数等），改为从样本描述表读取

--- 16s/PY_flash_dumx_to_joined.py
+++ 16s/PY_flash_dumx_to_joined.py
@@ -1,107 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Created Time  : 2026/01/04 17:52
+# Author        : William GoGo, Yu WangYin
+import os, sys
 import argparse
-import os
-from shutil import copyfile
-import sys
+from loguru import logger
+
+sys.path.append('/home/colddata/qinqiang/script/CommonTools/')
+from load_input import load_table
 
 
-parser=argparse.ArgumentParser(description="dumx_to_join")
-parser.add_argument('--inputPath',dest='inputPath',default='input_data1',
-        help='path of input file')
-
-parser.add_argument('--outputPath',dest='outputPath',default='NA',
-        help='path of output file')
-
-
-
-arg=parser.parse_args()
-if len(sys.argv)==1:
-    parser.print_help()
-    sys.exit(1)
-
-inputfilePath=arg.inputPath
-outputfilePath=arg.outputPath
-if inputfilePath=='input_data1':
-    os.system('echo need to input inputfilePath')
-    sys.exit()
-if outputfilePath=='NA':
-    outputfilePath='./01_1_single_rawdata'
-    if not os.path.exists(outputfilePath):
-        os.makedirs(outputfilePath)
-    os.system('echo your outputfilePath is '+outputfilePath)
+def parse_input():
+    parser = argparse.ArgumentParser(description="dumx_to_join")
+    parser.add_argument('-i', '--input-dir', required=True, help='输入数据目录，包含R1和R2文件')
+    parser.add_argument('-o', '--output-dir', required=True, help='输出数据目录')
+    parser.add_argument('-s', '--sample-info', required=True, help='样本描述表文件路径，包含 group, sample, R1, R2 列')
+    return parser.parse_args()
 
 
-
-    command=arg.type+' -db '+Path+' -query '+inputfilePath+' -out '+outputfilePath+os.sep+outputname+' -max_target_seqs '+arg.max_target_seqs+' -evalue 1e-5 -num_threads '+arg.threads+' -outfmt "6 qacc sacc pident qcovs qcovhsp ppos length mismatch gapopen qstart qend sstart send evalue bitscore stitle staxid ssciname" '
-    os.system(command)
-    os.system('echo running '+arg.type+' '+database)
-
-
-def R(Rname):
-    if 'R' in Rname:
-        return Rname
-    else:
-        return 'R'+Rname
-
-def splice(filename):
-    outputlist=[]
-    for i in filename.split('.'):
-        for y in i.split('_'):
-            for x in y .split('-'):
-                outputlist.append(x)
-    return outputlist
-
-shortnamelist=[]
-fullnamelist=[]
-
-filelist=os.listdir(inputfilePath)
-for filename in filelist:
-    partlist=splice(filename)
-    times=-1
-    if partlist[times]=='gz':
-        tail='.'.join(partlist[-2:])
-        times=-3
-    else:
-        tail=partlist[times]
-        times=-2
-
-    Rname=R(partlist[times])
-    Rstation=times
-    times-=1
-
-    while True:
-        if times==0-len(partlist):
-            shortname='-'.join(partlist[times:Rstation])
-            shortnamelist.append(shortname)
-            fullnamelist.append(filename)
-            break
-        if partlist[times].isdecimal() or partlist[times]=='16S' or partlist[times]=='16s' or partlist[times]=='18S' or partlist[times]=='18s' or partlist[times]=='ITS':
-            times-=1
-            continue
+def main():
+    args = parse_input()
+    
+    input_dir = args.input_dir
+    output_dir = args.output_dir
+    sample_info_file = args.sample_info
+    
+    # 创建输出目录
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        logger.info(f'创建输出目录: {output_dir}')
+    
+    # 读取样本描述表
+    logger.info(f'读取样本描述表: {sample_info_file}')
+    samples_df = load_table(sample_info_file)
+    
+    # 检查必需的列是否存在
+    required_columns = ['group', 'sample', 'R1', 'R2']
+    missing_columns = [col for col in required_columns if col not in samples_df.columns]
+    if missing_columns:
+        logger.error(f'样本描述表缺少必需的列: {missing_columns}')
+        logger.error(f'当前表的列: {list(samples_df.columns)}')
+        sys.exit(1)
+    
+    # 遍历每一行，处理每个样本
+    logger.info(f'开始处理 {len(samples_df)} 个样本')
+    for idx, row in samples_df.iterrows():
+        group = str(row['group']).strip()
+        sample = str(row['sample']).strip()
+        r1_file = str(row['R1']).strip()
+        r2_file = str(row['R2']).strip()
+        
+        # 构建完整的文件路径
+        r1_path = os.path.join(input_dir, r1_file)
+        r2_path = os.path.join(input_dir, r2_file)
+        
+        # 构建输出文件名（使用group或sample）
+        output_prefix = os.path.join(output_dir, sample)
+        
+        # 构建flash命令
+        command = f'flash {r1_path} {r2_path} -p 33 -r 250 -f 500 -s 100 -o {output_prefix}'
+        
+        logger.info(f'处理样本: {sample} (group: {group})')
+        logger.debug(f'执行命令: {command}')
+        
+        # 执行flash命令
+        exit_code = os.system(command)
+        if exit_code == 0:
+            logger.success(f'完成样本: {sample} (group: {group})')
         else:
-            shortname='-'.join(partlist[times:Rstation])
-            shortnamelist.append(shortname)
-            fullnamelist.append(filename)
-            break
+            logger.error(f'处理样本失败: {sample} (group: {group}), 退出码: {exit_code}')
+    
+    logger.success('所有样本处理完成!')
 
-used=[]
-times=0
-while times<len(shortnamelist):
-    groupA=shortnamelist[times]
 
-    if groupA not in used:
-        add = 1
-        while times+add<len(shortnamelist):
-            groupB=shortnamelist[times+add]
-            if groupA == groupB:
-                used.append(groupA)
-                R1=inputfilePath+os.sep+fullnamelist[times]
-                R2=inputfilePath+os.sep+fullnamelist[times+add]
-                command='flash '+R1+' '+R2+' -p 33 -r 250 -f 500 -s 100 -o '+outputfilePath+os.sep+groupA
-                os.system(command)
-                os.system('echo running ' +groupA)
-                add+=1
-                break
-            else:
-                add+=1
-
-    times+=1
+if __name__ == '__main__':
+    main()
