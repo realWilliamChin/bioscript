0113a899c39935982994645a31719e6d42776942
WilliamGoGo <realwilliamchin@outlook.com>
2026-04-10 17:53:34 +0800

    [update] Analysis/enrich_analysis/geneids_enrich_data_merge.py 优化批量富集结果合并脚本，支持GO/KEGG结果批量读取、显著性筛选、自动汇总为多sheet Excel文件

--- Analysis/enrich_analysis/geneids_enrich_data_merge.py
+++ Analysis/enrich_analysis/geneids_enrich_data_merge.py
@@ -13,19 +13,14 @@ from load_input import load_table
 
 def parse_input():
     p = argparse.ArgumentParser()
-    p.add_argument('-i', '--input-dir', dest='input_dir', default=os.getcwd(), type=str,
-                   help='input data dir')
-    p.add_argument('-o', '--output-file', dest='output_file', type=str,
-                   default='00_Enrichment_significant_pathway_summary.xlsx',
-                   help='output file endswith .xlsx, GO in sheet1, KEGG in sheet2')
-    p.add_argument('--kegg-pvalue-cutoff', dest='kegg_pvalue_cutoff', type=float,
-                   default=0.05, help='< kegg pvalue cutoff')
-    p.add_argument('--kegg-count-cutoff', dest='kegg_count_cutoff', type=int,
-                   default=2, help='> kegg count cutoff')
-    p.add_argument('--go-padjust-cutoff', dest='go_padjust_cutoff', type=float,
-                   default=0.05, help='< go padjust cutoff')
-    p.add_argument('--go-count-cutoff', dest='go_count_cutoff', type=int,
-                   default=2, help='> go count cutoff')
+    p.add_argument('-i', '--input-dir', default=os.getcwd(), type=str, help='input data dir')
+    p.add_argument('-o', '--output-file', type=str, default='00_Enrichment_significant_pathway_summary.xlsx', help='output file endswith .xlsx, GO in sheet1, KEGG in sheet2')
+    p.add_argument('--kegg-cutoff-pcol', choices=['pvalue', 'p.adjust'], default='pvalue', help='kegg 富集分析结果中用于显著性筛选的列')
+    p.add_argument('--kegg-cutoff-value', type=float, default=0.05, help='kegg 富集分析结果中用于显著性筛选的阈值')
+    p.add_argument('--kegg-count-cutoff', type=int, default=2, help='kegg 富集分析结果中用于显著性筛选的Count最小值')
+    p.add_argument('--go-cutoff-pcol', choices=['p.adjust', 'pvalue'], default='p.adjust', help='go 富集分析结果中用于显著性筛选的列')
+    p.add_argument('--go-cutoff-value', type=float, default=0.05, help='go 富集分析结果中用于显著性筛选的阈值')
+    p.add_argument('--go-count-cutoff', type=int, default=2, help='go 富集分析结果中用于显著性筛选的Count最小值')
     
     return p.parse_args()
 
@@ -49,15 +44,15 @@ def main():
     go_summary_df = pd.concat(go_enrich_df_list)
     kegg_summary_df = pd.concat(kegg_enrich_df_list)
     
-    go_summary_df = go_summary_df[go_summary_df['p.adjust'] < args.go_padjust_cutoff]
+    go_summary_df = go_summary_df[go_summary_df[args.go_cutoff_pcol] < args.go_cutoff_value]
     go_summary_df = go_summary_df[go_summary_df['Count'] > args.go_count_cutoff]
-    kegg_summary_df = kegg_summary_df[kegg_summary_df['pvalue'] < args.kegg_pvalue_cutoff]
+    kegg_summary_df = kegg_summary_df[kegg_summary_df[args.kegg_cutoff_pcol] < args.kegg_cutoff_value]
     kegg_summary_df = kegg_summary_df[kegg_summary_df['Count'] > args.kegg_count_cutoff]
         
     with pd.ExcelWriter(args.output_file, engine='openpyxl') as w:
         go_summary_df.to_excel(w, sheet_name='GO_analysis', index=False)
         kegg_summary_df.to_excel(w, sheet_name='KEGG_analysis', index=False)
-    
+
 
 if __name__ == '__main__':
     main()
