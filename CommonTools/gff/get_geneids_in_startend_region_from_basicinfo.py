e778bb22896a35181e165f88bcab7de352ae8fcb
WilliamGoGo <realwilliamchin@outlook.com>
2026-05-14 15:22:31 +0800

    [FIX] CommonTools/gff/get_geneids_in_startend_region_from_basicinfo.py 优化基因组区域基因筛选功能，支持保留输入区域的所有关联列，修复染色体数据类型匹配问题

--- CommonTools/gff/get_geneids_in_startend_region_from_basicinfo.py
+++ CommonTools/gff/get_geneids_in_startend_region_from_basicinfo.py
@@ -7,10 +7,13 @@ import argparse
 import pandas as pd
 from loguru import logger
 
+sys.path.append('/home/colddata/qinqiang/script/CommonTools')
+from load_input import load_table, write_output_df
+
 
 def parse_input():
     p = argparse.ArgumentParser()
-    p.add_argument('-i', '--input', help='输入表格格式文件，包含 chrom region_start 和 region_end，其他列自动保留')
+    p.add_argument('-i', '--input', help='输入表格格式文件，包含 CHROM region_start 和 region_end，其他列自动保留')
     p.add_argument('-b', '--basicinfo', help='输入 basicinfo 文件')
     p.add_argument('-o', '--output', help='输出文件')
     
@@ -22,21 +25,16 @@ def main():
     
     result_df_lst = []
     
-    df = pd.read_csv(args.input)
+    df = load_table(args.input, dtype={'CHROM': str, 'region_start': int, 'region_end': int})
     df_columns = df.columns.tolist()
-    # GeneID Chr_Number Start End Strand Gene_Def
-    basic_info_df = pd.read_csv(args.basicinfo, sep='\t')
-    basic_info_df['Chr_Number'] = basic_info_df['Chr_Number'].astype(str)
-    df['CHROM'] = df['CHROM'].astype(str)
-    
-    # print(df.dtypes)
-    # print(basic_info_df.dtypes)
+    # GeneID Chromosome Start End Strand Gene_Def
+    basic_info_df = load_table(args.basicinfo, quoting=3, dtype={'Chromosome': str, 'Start': int, 'End': int})
     
     for each_row in df.itertuples():
         each_row_df = basic_info_df[
             (each_row.region_start <= basic_info_df['Start']) &
             (each_row.region_end >= basic_info_df['End']) &
-            (each_row.CHROM == basic_info_df['Chr_Number'])
+            (each_row.CHROM == basic_info_df['Chromosome'])
         ].copy()
         # 追加其他所有信息
         for column in df_columns:
@@ -49,7 +47,7 @@ def main():
         logger.error('返回为空')
         sys.exit(1)
     
-    result_df.to_csv(args.output, index=False)
+    write_output_df(result_df, args.output, index=False)
 
 
 if __name__ == '__main__':
