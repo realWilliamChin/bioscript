4fda91f4279740b7072611e1b75bbd6e68e61d76
WilliamGoGo <realwilliamchin@outlook.com>
2026-06-26 14:38:03 +0800

    [UPDATE] transcriptome/each_group_filter_data.py 改用CommonTools的load_table/write_output_df,清理venn相关调试代码;CLI变更:位置参数改为-i/-o选项参数,移除--venn

--- transcriptome/each_group_filter_data.py
+++ transcriptome/each_group_filter_data.py
@@ -5,17 +5,18 @@
 import os, sys
 import pandas as pd
 import argparse
-import subprocess
 from loguru import logger
+
+sys.path.append('/home/colddata/qinqiang/script/CommonTools/')
+from load_input import load_table, write_output_df
 
 
 def parse_input():
     parser = argparse.ArgumentParser()
-    parser.add_argument(dest='input_file', help='输入文件，通常是 fpkm_matrix 或者是 reads_matrix 文件')
-    parser.add_argument(dest='output_dir', help='输出目录，输出文件按照组名称命名')
+    parser.add_argument('-i', '--input-file', help='输入文件，通常是 fpkm_matrix 或者是 reads_matrix 文件')
+    parser.add_argument('-o', '--output-dir', help='输出目录，输出文件按照组名称命名')
     parser.add_argument('-s', '--samples', help='samples_described.txt')
-    parser.add_argument('--filter-value', dest='filter_value', type=float, default=50, help='过滤值，默认大于等于 50')
-    parser.add_argument('--venn', action='store_true', help='加上这个参数，出 Venn 图')
+    parser.add_argument('-f', '--filter-value', type=float, default=50, help='过滤值，默认大于等于 50')
     
     args = parser.parse_args()
     
@@ -24,32 +25,17 @@ def parse_input():
 
 if __name__ == '__main__':
     args = parse_input()
-    output_dir = args.output_dir
-    df = pd.read_csv(args.input_file, sep='\t')
-    df['GeneID'].astype(str)
-    samples_df = pd.read_csv(args.samples, sep='\t', usecols=[0, 1])
-
-    all_group_list = samples_df.drop_duplicates(subset='group')['group'].tolist()
+    df = load_table(args.input_file)
+    df[df.columns[0]].astype(str)
+    samples_df = load_table(args.samples, usecols=[0, 1])
+    
+    all_group_list = samples_df['group'].drop_duplicates().tolist()
     grouped_samples_df = samples_df.groupby(by='group')
-
-    dataframe_list = []
     for each_group in all_group_list:
-        each_group_sample_list = grouped_samples_df.get_group(each_group)['sample'].tolist()
-        each_group_sample_df = df[['GeneID'] + each_group_sample_list].copy()
+        each_group_sample_list = grouped_samples_df.get_group(each_group)['sample'].values.tolist()
+        each_group_sample_df = df[[df.columns[0]] + each_group_sample_list].copy()
         each_group_sample_df['max'] = each_group_sample_df.max(axis=1, numeric_only=True)
-        each_group_sample_df = each_group_sample_df[each_group_sample_df['max'] >= args.filter_value]
-        each_group_sample_df = each_group_sample_df.drop(columns=['max'])
+        each_group_sample_df = each_group_sample_df[each_group_sample_df['max'] >= args.filter_value].drop(columns=['max'])
         logger.info(f'{each_group}, {each_group_sample_df.shape[0]}')
-        each_group_sample_df_filename = f'{each_group}.txt'
-        each_group_sample_df.to_csv(os.path.join(output_dir, each_group_sample_df_filename), sep='\t', index=False)
-        each_group_sample_df = each_group_sample_df.drop(columns=each_group_sample_list)
-        each_group_sample_df = each_group_sample_df.rename(columns={"GeneID": each_group})
-        dataframe_list.append(each_group_sample_df)
-        
-
-    names_list = [x.columns[0] for x in dataframe_list]
-    venn_df = pd.concat(dataframe_list, axis=1, names=names_list)
-    print(venn_df)
-    venn_df.to_csv('venn.txt', sep='\t', index=False)
-    logger.success('Done')
-
+        each_group_sample_df_filename = os.path.join(args.output_dir, f'{each_group}.txt')
+        write_output_df(each_group_sample_df, each_group_sample_df_filename, index=False)
